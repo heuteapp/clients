98a47e4e669620a7701aefeb247f8ff2c3e6042e
k4yr2 <kyrzkaya@gmail.com>
2026-02-18 02:28:56 +0300

    feat: initialize default layout in HeuteDayboard constructor

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,20 @@ class HeuteDayboard {
     #layouts = new HeuteDayboardLayouts();
 
     constructor() {
+        this.#layouts.add(
+            createLayout("default", {
+                grids: [
+                    createGrid("first", {
+                        size: { cols: 12, rows: 6 },
+                        bounds: { x1: 0, y1: 0, x2: 100, y2: 50 },
+                    }),
+                    createGrid("second", {
+                        size: { cols: 12, rows: 6 },
+                        bounds: { x1: 0, y1: 50, x2: 100, y2: 100 },
+                    }),
+                ],
+            })
+        );
     }
 
     public get layouts(): HeuteDayboardLayouts {
