674ddec6ecd35e96a7ddb5ce7d5d5986531f523a
k4yr2 <kyrzkaya@gmail.com>
2026-02-20 04:05:02 +0300

    refactor: reorganize mergeRefs utility for improved import structure

--- src/components/dayboard.tsx
+++ src/components/dayboard.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { createContext, forwardRef, use, useContext, useEffect, useLayoutEffect, useRef } from "react";
+import { createContext, forwardRef, useContext, useEffect, useLayoutEffect, useRef } from "react";
 import styles from "./dayboard.module.css";
-import mergeRefs from "merge-refs";
+import { mergeRefs } from "@/src/utility";
 import { useReadyRef } from "../hooks";
 import { DayboardData, DayboardFieldData, DayboardGridData, DayboardLayoutData } from "@/src/data/dayboard";
 import { GridSize } from "../data/core";
