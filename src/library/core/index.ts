4c36348f8128613c11644107db1e709fe8ff87d4
k4yr2 <kyrzkaya@gmail.com>
2026-02-20 07:25:34 +0300

    feat: add core data types and utility functions to index

---
+++ src/library/core/index.ts
@@ -0,0 +1,12 @@
+export type { default as Point } from "./point";
+export type { default as Size } from "./size";
+export type { default as GridSize } from "./gridSize";
+export type { default as Rect } from "./rect";
+export type { default as Bounds } from "./bounds";
+export type { default as Edges } from "./edges";
+export type { default as Alignment } from "./alignment";
+export type { default as Placement } from "./placement";
+export type { default as UniqueData } from "./uniqueData";
+export type { default as DataWithoutId } from "./dataWithoutId";
+
+export { assignData, assignDataWithId } from "./dataWithoutId";
