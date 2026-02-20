2960e12a457660c591c4a2281f32396c036bd2bc
k4yr2 <kyrzkaya@gmail.com>
2026-02-20 09:26:07 +0300

    feat: create DayboardFieldModel interface for grid and placement representation

---
+++ src/library/models/boardField.ts
@@ -0,0 +1,8 @@
+import { UniqueData, Bounds, Placement } from "@/src/library/base";
+import BoardGridModel from "./boardGrid";
+
+export interface DayboardFieldModel extends UniqueData {
+    grid: BoardGridModel;
+    bounds: Bounds;
+    placement?: Placement;
+}
