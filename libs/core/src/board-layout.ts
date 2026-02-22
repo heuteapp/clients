07a92073742e4e3d2e55f6954e70be374eb0c494
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 06:21:32 +0300

    feat: implement HeuteBoardLayout class with private board property and getter

--- libs/core/src/board-layout.ts
+++ libs/core/src/board-layout.ts
@@ -1,4 +1,15 @@
+import { HeuteBoard } from "./board";
+
 export class HeuteBoardLayout {
+    #board: HeuteBoard;
+    
+    constructor(board: HeuteBoard) {
+        this.#board = board;
+    }
+
+    public get board(): HeuteBoard {
+        return this.#board;
+    }
 }
 
 export default HeuteBoardLayout;
