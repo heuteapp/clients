f6fdf09f6faa8a96dd2d55f37844bfb0c3ea52d4
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 09:12:38 +0300

    refactor: clean up formatting and improve method naming in HeuteBoardSection class

--- libs/core/src/board-section.ts
+++ libs/core/src/board-section.ts
@@ -4,7 +4,7 @@ import { HeuteBoardLayout } from "./board-layout";
 export class HeuteBoardSection {
     readonly #id : string;
     readonly #layout : HeuteBoardLayout;
-    
+
     #cols : number;
     #rows : number;
     #position : Rect = { x: 0, y: 0, width: 0, height: 0 };
@@ -17,28 +17,56 @@ export class HeuteBoardSection {
         this.#rows = rows;
     }
 
-    public get id() : string {
+    public getId() : string {
         return this.#id;
     }
 
-    public get layout() : HeuteBoardLayout {
+    public getLayout() : HeuteBoardLayout {
         return this.#layout;
     }
 
+    //
+
     public get cols() : number {
         return this.#cols;
+    }
+
+    public set cols(cols: number) {
+        if(cols < 1) {
+            this.#cols = 1;
+            return;
+        }
+
+        this.#cols = cols;
     }
 
     public get rows() : number {
         return this.#rows;
     }
 
+    public set rows(rows: number) {
+        if(rows < 1) {
+            this.#rows = 1;
+            return;
+        }
+
+        this.#rows = rows;
+    }
+
     public get position() : Rect {
         return this.#position;
+    }
+
+    public set position(position: Rect) {
+        this.#position = position;
     }
     
     public get placement() : Placement {
         return this.#placement;
+    }
+
+    public set placement(placement: Placement) {
+        this.#placement = placement;
     }
 }
 
