8f45267eb94f77ad3e65c017023a96682d04222e
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 06:23:01 +0300

    feat: add layout property and methods to HeuteBoard class

--- libs/core/src/board.ts
+++ libs/core/src/board.ts
@@ -1,4 +1,19 @@
+import HeuteBoardLayout from "./board-layout";
+
 export class HeuteBoard {
+    #layout: HeuteBoardLayout | null;
+
+    constructor(layout?: HeuteBoardLayout) {
+        this.#layout = layout || null;
+    }
+
+    public get layout(): HeuteBoardLayout | null {
+        return this.#layout;
+    }
+
+    public setLayout(layout: HeuteBoardLayout) {
+        this.#layout = layout;
+    }
 }
 
 export default HeuteBoard;
