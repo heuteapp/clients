b75ffea1758e9cfa65b6eee35d8132a1eb6a36db
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 06:26:59 +0300

    feat: add id property and getter to HeuteBoardField class

--- libs/core/src/board-field.ts
+++ libs/core/src/board-field.ts
@@ -2,12 +2,18 @@ import { HeuteBoardGrid } from "./board-grid";
 import { HeuteBoardLayout } from "./board-layout";
 
 export class HeuteBoardField {
+    readonly #id: string;
     readonly #layout: HeuteBoardLayout;
     readonly #grid: HeuteBoardGrid;
 
-    constructor(layout: HeuteBoardLayout, cols: number, rows: number) {
+    constructor(id: string, layout: HeuteBoardLayout, cols: number, rows: number) {        
+        this.#id = id;
         this.#layout = layout;
         this.#grid = new HeuteBoardGrid(this, cols, rows);
+    }
+
+    public get id() {
+        return this.#id;
     }
 
     public get layout() {
