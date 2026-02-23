95c002a4c6283555cc6f25d2648926213eb7e07b
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 00:19:10 +0300

    refactor: simplify #normalizePosition method by removing unnecessary conditional block

--- libs/domain/src/board-card.ts
+++ libs/domain/src/board-card.ts
@@ -127,9 +127,8 @@ export class HeuteBoardCard {
     }
 
     #normalizePosition(position?: GridRect | null) : GridRect | null {
-        if(!position) {
-            return null;
-        }
+        if(!position) return null;
+        
         return Object.freeze({ 
             col: Math.max(0, position.col),
             row: Math.max(0, position.row),
