26c1410c1da7af18f180c57386255d49f16f56fd
k4yr2 <kyrzkaya@gmail.com>
2026-02-23 09:18:23 +0300

    refactor: add id property to HeuteBoard and update constructor to accept id parameter

--- libs/domain/src/board.ts
+++ libs/domain/src/board.ts
@@ -2,18 +2,30 @@ import { HeuteBoardCard } from "./board-card";
 import { HeuteBoardLayout } from "./board-layout";
 
 export class HeuteBoard {
+    #id: string;
     #layout: HeuteBoardLayout;
     #cards: HeuteBoardCard[];
 
-    constructor(layout: HeuteBoardLayout) {
-        this.#layout = layout;
-        this.#cards = [];
+    constructor(id: string, props: HeuteBoardProps) {
+        this.#id = id;
+        this.#layout = props.layout;
+        this.#cards = props.cards || [];
     }
 
     //
 
+    public get id() : string {
+        return this.#id;
+    }
+
     public get layout(): HeuteBoardLayout {
         return this.#layout;
+    }
+
+    //
+
+    public set id(id: string) {
+        this.#id = id;
     }
     
     //
@@ -22,9 +34,18 @@ export class HeuteBoard {
         this.#layout = HeuteBoardLayout.copy(layout);
     }
 
+    public addCard(card: HeuteBoardCard) {
+        this.#cards.push(card);
+    }
+
     public listCards(): ReadonlyArray<HeuteBoardCard> {
         return [...this.#cards];
     }
 }
 
 export default HeuteBoard;
+
+export interface HeuteBoardProps {
+    layout: HeuteBoardLayout;
+    cards?: HeuteBoardCard[];
+}
