2a987f06a49a598325ceb0b6c32f0984cd5e7362
k4yr2 <kyrzkaya@gmail.com>
2026-02-23 07:25:35 +0300

    feat: update constructor in HeuteBoardLayout to accept props for sections

--- libs/domain/src/board-layout.ts
+++ libs/domain/src/board-layout.ts
@@ -4,9 +4,9 @@ export class HeuteBoardLayout {
     readonly #id: string;
     readonly #sections: HeuteBoardSection[];
 
-    constructor(id: string, sections: HeuteBoardSection[]) {
+    constructor(id: string, props: HeuteBoardLayoutProps) {
         this.#id = id;
-        this.#sections = sections;
+        this.#sections = props.sections;
     }
 
     public get id(): string {
@@ -23,8 +23,12 @@ export class HeuteBoardLayout {
 
     public static copy(layout: HeuteBoardLayout): HeuteBoardLayout {
         const sectionsCopy = layout.getSections().map(section => HeuteBoardSection.copy(section));
-        return new HeuteBoardLayout(layout.id, sectionsCopy);
+        return new HeuteBoardLayout(layout.id, { sections: sectionsCopy });
     }
 }
 
 export default HeuteBoardLayout;
+
+export interface HeuteBoardLayoutProps {
+    sections: HeuteBoardSection[];
+}
