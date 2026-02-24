684ca9da579814484481066956b21bf9a8697ec2
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 03:41:58 +0300

    refactor: remove clampNumber utility function and its export

--- libs/utility/src/clampNumber.ts
+++
@@ -1,5 +0,0 @@
-export function clampNumber(n: number, min: number, max: number) {
-    return Math.max(min, Math.min(max, n));
-}
-
-export default clampNumber;
--- libs/utility/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { clampNumber } from "./clampNumber";
