878162ded945647c46d2d2c8af14323dddc48d3d
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 03:41:15 +0300

    refactor: implement isRectOverlapping function for rectangle overlap detection

--- libs/common/src/rect.ts
+++ libs/common/src/rect.ts
@@ -3,3 +3,14 @@ import { Size } from "./size";
 
 export interface Rect extends Point, Size {
 }
+
+export default Rect;
+
+export function isRectOverlapping(rectA: Rect, rectB: Rect): boolean {
+    return (
+        rectA.x < rectB.x + rectB.width &&
+        rectA.x + rectA.width > rectB.x &&
+        rectA.y < rectB.y + rectB.height &&
+        rectA.y + rectA.height > rectB.y
+    );
+}
