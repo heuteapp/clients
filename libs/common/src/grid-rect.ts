39245d04a161f7a984bbde0f66870f37dd468b4c
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 03:40:08 +0300

    refactor: add isGridRectOverlapping function for grid rectangle overlap detection

--- libs/common/src/grid-rect.ts
+++ libs/common/src/grid-rect.ts
@@ -5,3 +5,12 @@ export interface GridRect extends GridPoint, GridPlacement {
 }
 
 export default GridRect;
+
+export function isGridRectOverlapping(rectA: GridRect, rectB: GridRect): boolean {
+    return (
+        rectA.col < rectB.col + rectB.colSpan &&
+        rectA.col + rectA.colSpan > rectB.col &&
+        rectA.row < rectB.row + rectB.rowSpan &&
+        rectA.row + rectA.rowSpan > rectB.row
+    );
+}
