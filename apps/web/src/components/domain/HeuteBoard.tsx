e7233b42adba9e00f28da2a995ec67ed77170739
k4yr2 <kyrzkaya@gmail.com>
2026-03-05 18:42:49 +0300

    feat: enhance HeuteLayout and HeuteBoard to support dynamic sections rendering

--- apps/web/src/components/domain/HeuteBoard.tsx
+++ apps/web/src/components/domain/HeuteBoard.tsx
@@ -13,7 +13,17 @@ interface HeuteBoardProps {
 export default function HeuteBoard({ category, date }: HeuteBoardProps) {
   return (
     <div className={style.board}>
-      <HeuteLayout />
+      <HeuteLayout columns={18} rows={8} sections={[{
+        colIndex: 1,
+        rowIndex: 1,
+        colSpan: 18,
+        rowSpan: 4
+      }, {
+        colIndex: 1,
+        rowIndex: 5,
+        colSpan: 18,
+        rowSpan: 4
+      }]} />
     </div>
   )
 }
--- apps/web/src/components/domain/HeuteLayout.tsx
+++ apps/web/src/components/domain/HeuteLayout.tsx
@@ -1,43 +1,82 @@
 "use client"
 
 import style from "@/src/styles/domain/HeuteLayout.module.css"
-import { forwardRef } from "react"
-
-//
+import { useMemo, useRef } from "react"
 
 interface HeuteLayoutProps {
-  sections: LayoutSectionProps[];
+  columns: number
+  rows: number
+  sections: LayoutSectionData[]
 }
 
-export default function HeuteLayout({ sections }: HeuteLayoutProps) {
+export default function HeuteLayout({
+  columns,
+  rows,
+  sections,
+}: HeuteLayoutProps) {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const squareSize = useMemo(() => {
+    if (!containerRef.current) return 0
+
+    const { clientWidth, clientHeight } = containerRef.current
+
+    return Math.min(
+      clientWidth / columns,
+      clientHeight / rows
+    )
+  }, [columns, rows])
+
   return (
-    <div className={style.layout}>
-      {sections.map((section, index) => (
-        <LayoutSection
-          key={index}
-          colIndex={section.colIndex}
-          rowIndex={section.rowIndex}
-          colSpan={section.colSpan}
-          rowSpan={section.rowSpan}
-        />
-      ))}
+    <div ref={containerRef} className={style.layout}>
+      {squareSize > 0 &&
+        sections.map((section, index) => (
+          <LayoutSection
+            key={index}
+            squareSize={squareSize}
+            {...section}
+          />
+        ))}
     </div>
   )
 }
 
-//
-
-interface LayoutSectionProps {
-  colIndex: number;
-  rowIndex: number;
-  colSpan: number;
-  rowSpan: number;
+interface LayoutSectionData {
+  colIndex: number
+  rowIndex: number
+  colSpan: number
+  rowSpan: number
 }
 
-const LayoutSection = forwardRef<HTMLDivElement, LayoutSectionProps>((props, ref) => {
-  return (
-    <div className={style.layoutSection} ref={ref}>
+interface LayoutSectionProps extends LayoutSectionData {
+  squareSize: number
+}
 
+function LayoutSection({
+  squareSize,
+  colIndex,
+  rowIndex,
+  colSpan,
+  rowSpan,
+}: LayoutSectionProps) {
+  return (
+    <div
+      className={style.layoutSection}
+      style={{
+        position: "absolute",
+        left: (colIndex -1)* squareSize,
+        top: (rowIndex -1)* squareSize,
+        width: (colSpan * squareSize) - 8,
+        height: (rowSpan * squareSize) - 8,
+        gridTemplateColumns: `repeat(${colSpan}, 1fr)`,
+        gridTemplateRows: `repeat(${rowSpan}, 1fr)`,
+      }}
+    >
+      {// do grid items
+        Array.from({ length: colSpan * rowSpan }, (_, i) => (
+          <div key={i} className={style.layoutSectionItem} />
+        ))
+      }
     </div>
-  );
-})
+  )
+}
