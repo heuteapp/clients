6e6100352516a1d6851c7064c282a1d1875a944f
k4yr2 <kyrzkaya@gmail.com>
2026-03-05 11:57:25 +0300

    feat: add "use client" directive to Monitor, Panel, and Sidebar components

--- apps/web/src/components/layout/Monitor.tsx
+++ apps/web/src/components/layout/Monitor.tsx
@@ -1,3 +1,4 @@
+"use client";
 import style from '@/src/styles/layout/Monitor.module.css';
 
 interface MonitorProps {
--- apps/web/src/components/layout/Sidebar.tsx
+++ apps/web/src/components/layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import style from '@/src/styles/layout/Sidebar.module.css';
 
 function Sidebar() {
