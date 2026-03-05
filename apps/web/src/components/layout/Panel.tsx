ab082274f00904a6dbbf688bbfc05df00475612d
k4yr2 <kyrzkaya@gmail.com>
2026-03-05 11:40:31 +0300

    feat: add Panel component with styling

---
+++ apps/web/src/components/layout/Panel.tsx
@@ -0,0 +1,11 @@
+import style from '@/src/styles/layout/Panel.module.css';
+
+function Panel() {
+  return (
+    <div className={style.panel}>
+        Panel
+    </div>
+  )
+}
+
+export default Panel
