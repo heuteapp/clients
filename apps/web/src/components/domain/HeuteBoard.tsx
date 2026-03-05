3210ae4230be8db88c7561cb196160f2194fe377
k4yr2 <kyrzkaya@gmail.com>
2026-03-06 00:06:31 +0300

    feat: update HeuteBoard to use sectionExamples for layout configuration

--- apps/web/src/components/domain/HeuteBoard.tsx
+++ apps/web/src/components/domain/HeuteBoard.tsx
@@ -13,22 +13,42 @@ interface HeuteBoardProps {
 export default function HeuteBoard({ category, date }: HeuteBoardProps) {
   return (
     <div className={style.board}>
-      <HeuteLayout columnCount={18} rowCount={8} sections={[{
-        colIndex: 1,
-        rowIndex: 1,
-        colSpan: 12,
-        rowSpan: 4
-      }, {
-        colIndex: 1,
-        rowIndex: 5,
-        colSpan: 12,
-        rowSpan: 4
-      }, {
-        colIndex: 13,
-        rowIndex: 1,
-        colSpan: 6,
-        rowSpan: 8
-      }]} />
+      <HeuteLayout columnCount={18} rowCount={8} sections={sectionExamples.two} />
     </div>
   )
+}
+
+const sectionExamples = { 
+  two:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 18,
+      rowSpan: 4
+    },
+    {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 18,
+      rowSpan: 4
+    }
+  ],
+  three:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 12,
+      rowSpan: 4
+    }, {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 12,
+      rowSpan: 4
+    }, {
+      colIndex: 13,
+      rowIndex: 1,
+      colSpan: 6,
+      rowSpan: 8
+    }
+  ]
 }
