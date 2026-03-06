90d627c6baa83cbc2f5db4299983b255e9efcf37
k4yr2 <kyrzkaya@gmail.com>
2026-03-06 13:57:55 +0300

    feat: refactor HeuteBoard to use external sectionExamples data and improve code organization

---
+++ apps/web/src/data/sectionExamples.ts
@@ -0,0 +1,129 @@
+export const sectionExamples = { 
+  two:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 18,
+      rowSpan: 4
+    },
+    {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 18,
+      rowSpan: 4
+    }
+  ],
+  three:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 12,
+      rowSpan: 4
+    }, {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 12,
+      rowSpan: 4
+    }, {
+      colIndex: 13,
+      rowIndex: 1,
+      colSpan: 6,
+      rowSpan: 8
+    }
+  ],
+  three2:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 15,
+      rowSpan: 4
+    }, {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 15,
+      rowSpan: 4
+    }, {
+      colIndex: 16,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }
+  ],
+  four:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 9,
+      rowSpan: 4
+    },
+    {
+      colIndex: 10,
+      rowIndex: 1,
+      colSpan: 9,
+      rowSpan: 4
+    },
+    {
+      colIndex: 1,
+      rowIndex: 5,
+      colSpan: 9,
+      rowSpan: 4
+    },
+    {
+      colIndex: 10,
+      rowIndex: 5,
+      colSpan: 9,
+      rowSpan: 4
+    }
+  ],
+  four2:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }, {
+      colIndex: 4,
+      rowIndex: 1,
+      colSpan: 12,
+      rowSpan: 4
+    },  {
+      colIndex: 4,
+      rowIndex: 5,
+      colSpan: 12,
+      rowSpan: 4
+    }, {
+      colIndex: 16,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }
+  ],
+  five:[
+    {
+      colIndex: 1,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }, {
+      colIndex: 4,
+      rowIndex: 1,
+      colSpan: 9,
+      rowSpan: 4
+    },  {
+      colIndex: 4,
+      rowIndex: 5,
+      colSpan: 9,
+      rowSpan: 4
+    }, {
+      colIndex: 13,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }, {
+      colIndex: 16,
+      rowIndex: 1,
+      colSpan: 3,
+      rowSpan: 8
+    }
+  ],
+}
