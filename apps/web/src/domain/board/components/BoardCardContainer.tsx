a88d765d6244fc10ba05cbf5139b3b4847c334bb
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 12:48:19 +0300

    refactor: integrate useBoardContext in BoardCardContainer for improved state management

--- apps/web/src/domain/board/components/BoardCardContainer.tsx
+++ apps/web/src/domain/board/components/BoardCardContainer.tsx
@@ -1,9 +1,16 @@
+import { useBoardContext } from "../board.hooks"
 import style from "../board.module.css"
 
 function BoardCardContainer(props : BoardCardContainerProps) {
+    const context = useBoardContext();
+
     return (
-        <div className={style.cardContainer} style={{
-        }}/>
+        <div 
+            className={style.cardContainer} 
+            style={{
+            }}
+        >
+        </div>
     )
 }
 
