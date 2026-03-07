b612876d3a083b1236c69d344459402952b6b9ab
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 11:45:31 +0300

    refactor: update BoardCardContainer to remove hidden visibility style

--- apps/web/src/domain/board/components/BoardCardContainer.tsx
+++ apps/web/src/domain/board/components/BoardCardContainer.tsx
@@ -3,7 +3,6 @@ import style from "../board.module.css"
 function BoardCardContainer(props : BoardCardContainerProps) {
     return (
         <div className={style.cardContainer} style={{
-            visibility: "hidden"
         }}/>
     )
 }
