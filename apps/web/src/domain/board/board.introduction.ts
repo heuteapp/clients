eb03e4fccd36e1c21ec35808d12b9442b7754f96
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 17:22:07 +0300

    feat: create BoardIntroduction interface and define BoardBaseState structure

---
+++ apps/web/src/domain/board/board.introduction.ts
@@ -0,0 +1,11 @@
+import { Pointer } from "@/src/types";
+
+export interface BoardIntroduction {
+
+}
+
+export interface BoardBaseState {
+    cardId: string;
+    sectionId: string;
+    startPointer: Pointer;
+}
