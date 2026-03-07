2ce369c92f35579286c674c989999451116d952f
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 17:21:34 +0300

    feat: add GridPosition and GridSize types for grid layout representation

--- apps/web/src/types.ts
+++ apps/web/src/types.ts
@@ -2,3 +2,13 @@ export type Pointer = {
     x: number;
     y: number;
 }
+
+export type GridPosition = {
+    rowIndex: number;
+    colIndex: number;
+}
+
+export type GridSize = {
+    rowSpan: number;
+    colSpan: number;
+}
