9b373fa9363235000ed267f552a26f328e6456ca
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 18:25:42 +0300

    feat: add useBoardInteraction hook for managing board interactions

--- apps/web/src/domain/board/board.hooks.ts
+++ apps/web/src/domain/board/board.hooks.ts
@@ -1,5 +1,6 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { HeuteBoardContext } from "./board.context"
+import { BoardInteraction, createBoardInteraction } from "./board.interaction"
 
 export function useBoardContext() {
     const ctx = useContext(HeuteBoardContext)
@@ -9,4 +10,14 @@ export function useBoardContext() {
     }
 
     return ctx
+}
+
+export function useBoardInteraction() : BoardInteraction {
+    const interaction = useRef<BoardInteraction>(null);
+
+    if(!interaction.current) {
+        interaction.current = createBoardInteraction();
+    }
+
+    return interaction.current;
 }
