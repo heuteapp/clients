cc64c0099ac38673a7c5805a0aadbb9bf64b4061
k4yr2 <kyrzkaya@gmail.com>
2026-03-08 00:55:58 +0300

    refactor: change Pointer, GridPosition, and GridSize from type to interface for improved type safety

--- apps/web/src/types.ts
+++ apps/web/src/types.ts
@@ -1,16 +1,18 @@
-export type Pointer = {
+export interface Pointer {
     x: number;
     y: number;
 }
 
-export type GridPosition = {
+export interface GridPosition {
     rowIndex: number;
     colIndex: number;
 }
 
-export type GridSize = {
+export interface GridSize {
     rowSpan: number;
     colSpan: number;
 }
+
+export interface GridRect extends GridPosition, GridSize {}
 
 export type ResizeHandle = "n" | "e" | "s" | "w" | "ne" | "nw" | "se" | "sw"
