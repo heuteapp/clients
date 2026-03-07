fa33ec582199eed2f638d82487b7a8a2b8ede02f
k4yr2 <kyrzkaya@gmail.com>
2026-03-07 21:42:27 +0300

    refactor: enhance Sidebar component layout and structure

--- apps/web/src/body/components/Sidebar.tsx
+++ apps/web/src/body/components/Sidebar.tsx
@@ -1,10 +1,25 @@
 "use client";
+import { useBoardContext } from '@/src/domain/board/board.hooks';
 import style from '../body.module.css';
 
 function Sidebar() {
+  const context = useBoardContext();
+
   return (
     <div className={style.sidebar}>
-        
+      <div
+        style={{
+          width:48,
+          height:48,
+          backgroundColor: '#ffffff',
+          border: '1px solid #cccccc',
+        }}
+        onPointerDown={() => {
+          context.interaction.startCardCreate({ colSpan: 3, rowSpan: 2 });
+        }}
+      >
+
+      </div>
     </div>
   )
 }
