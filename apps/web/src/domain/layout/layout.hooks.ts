708e07c514beed3dfcbd9b99b9bbeb9917537f9c
k4yr2 <kyrzkaya@gmail.com>
2026-03-08 18:48:10 +0300

    feat: move calculateSectionCount function to section-count.ts and remove old utils file for better organization

--- apps/web/src/domain/layout/layout.hooks.ts
+++ apps/web/src/domain/layout/layout.hooks.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react"
 import { LayoutRegistry } from "../board/board.registry"
 import { LayoutMeasurements } from "./types/layout.dom.types"
 import { LayoutSectionData } from "./types/layout.data.types"
-import { calculateSectionCount } from "./utils/section-count.utils"
+import { calculateSectionCount } from "./utils/section-count"
 import { LayoutCellNode, LayoutGridNode, LayoutSectionNode } from "./types/layout.registry.types"
 
 export function useLayoutRegistry(): LayoutRegistry {
