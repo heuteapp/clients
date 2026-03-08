16d5f282d5f6e8210e0608c865342905fdee4df7
k4yr2 <kyrzkaya@gmail.com>
2026-03-08 22:40:58 +0300

    refactor: update Sidebar component structure and styling for better organization

--- apps/web/src/body/components/Sidebar.tsx
+++ apps/web/src/body/components/Sidebar.tsx
@@ -1,29 +1,45 @@
 "use client";
 import { useBoardContext } from '@/src/domain/board/board.hooks';
 import style from '../body.module.css';
+import { BoardContextValue } from '@/src/domain/board/board.context';
 
 function Sidebar() {
-  const context = useBoardContext();
 
   return (
     <div className={style.sidebar}>
-      <div
-        style={{
-          width:48,
-          height:48,
-          backgroundColor: '#ffffff',
-          border: '1px solid #cccccc',
-          userSelect: 'none',
-          cursor: context.interaction.eventType === "create" ? "default" : "pointer",
-        }}
-        onPointerDown={() => {
-          context.interaction.startCardCreate({ colSpan: 9, rowSpan: 2 });
-        }}
-      >
-
-      </div>
+      <SidebarItem onPointerDown={(context) => {
+          context.interaction.startCardCreate({
+            colSpan: 9,
+            rowSpan: 4,
+          });
+      }} />
+      <SidebarItem onPointerDown={(context) => {
+          context.interaction.startCardCreate({
+            colSpan: 6,
+            rowSpan: 4,
+          });
+      }} />
     </div>
   )
 }
 
 export default Sidebar;
+
+function SidebarItem({ onPointerDown } : { onPointerDown: (context: BoardContextValue) => void }) {
+    const context = useBoardContext();
+
+  return (
+    <div
+      style={{
+        width:48,
+        height:48,
+        backgroundColor: '#ffffff',
+        border: '1px solid #cccccc',
+        userSelect: 'none',
+        cursor: context.interaction.eventType === "create" ? "default" : "pointer",
+      }}
+      onPointerDown={() => { onPointerDown(context); }}
+    >
+    </div>
+  )
+}
