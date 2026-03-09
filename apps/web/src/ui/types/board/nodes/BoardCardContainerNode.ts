18271a36895a5ed00ca3e2f54d8f7136d1d44805
k4yr2 <kyrzkaya@gmail.com>
2026-03-09 16:37:31 +0300

    feat: add BoardCardContainerNode interface extending RegistryBaseNode with optional props

---
+++ apps/web/src/ui/types/board/nodes/BoardCardContainerNode.ts
@@ -0,0 +1,6 @@
+import { BoardCardContainerProps } from "@/src/ui/components/board/BoardCardContainer";
+import { RegistryBaseNode } from "@/src/ui/types/shared/registry";
+
+export interface BoardCardContainerNode extends RegistryBaseNode {
+    props?: BoardCardContainerProps; 
+}
