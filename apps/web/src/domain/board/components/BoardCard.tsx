65d721ff708f6a8ff11e98d6c01bd277f906c3f9
k4yr2 <kyrzkaya@gmail.com>
2026-03-09 10:23:28 +0300

    fix: refactor position calculation in BoardCard for improved layout accuracy

--- apps/web/src/domain/board/components/BoardCard.tsx
+++ apps/web/src/domain/board/components/BoardCard.tsx
@@ -9,20 +9,53 @@ function BoardCard(props : BoardCardProps) {
     const section = registry.getLayoutGrid(props.sectionId);
     if(!section) return null;
 
+
+    if(!measurements) return null;
+
     const rootRect = registry.layout!.ref!.current!.getBoundingClientRect();
     const sectionRect = section.ref!.current!.getBoundingClientRect();
 
-    const left = sectionRect.left - rootRect.left;
-    const top = sectionRect.top - rootRect.top;
+    const gridSize = {
+        width: measurements.gridSize.maxWidth / (measurements.cellCount.horizontal / section.props!.colSpan),
+        height: measurements.gridSize.maxHeight / (measurements.cellCount.vertical / section.props!.rowSpan)
+    }
+
+    const gap = 6;
+
+    const gridRect = {
+        left: (sectionRect.left - rootRect.left) + gap,
+        top: (sectionRect.top - rootRect.top) + gap,
+        width: gridSize.width - gap * 2,
+        height: gridSize.height - gap * 2
+    }
+
+    const stepSize = {
+        width: gridRect.width / section.props!.colSpan,
+        height: gridRect.height / section.props!.rowSpan
+    }
+
+    const rawPosition = {
+        left: gridRect.left + (props.colIndex - 1) * stepSize.width,
+        top: gridRect.top + (props.rowIndex - 1) * stepSize.height,
+        width: props.colSpan * stepSize.width,
+        height: props.rowSpan * stepSize.height,
+    }
+
+    const position = {
+        left: rawPosition.left + gap,
+        top: rawPosition.top + gap,
+        width: rawPosition.width - gap * 2,
+        height: rawPosition.height - gap * 2
+    }
 
     return (
         <div 
             className={style.card} 
             style={{
-                left: 6 + left + (props.colIndex - 1) * measurements!.cellSize.inner,
-                top: 6 + top + (props.rowIndex - 1) * measurements!.cellSize.inner,
-                width: props.colSpan * measurements!.cellSize.inner - 12,
-                height: props.rowSpan * measurements!.cellSize.inner - 12,
+                left: position.left,
+                top: position.top,
+                width: position.width,
+                height: position.height
             }}
         >
         </div>
