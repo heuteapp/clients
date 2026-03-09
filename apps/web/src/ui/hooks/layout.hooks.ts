6c432479d9fb71a23825728272d68d4c5c002f22
k4yr2 <kyrzkaya@gmail.com>
2026-03-09 15:45:09 +0300

    feat: refactor useLayoutMeasurements to utilize measurementsRef for state management

--- apps/web/src/ui/hooks/layout.hooks.ts
+++ apps/web/src/ui/hooks/layout.hooks.ts
@@ -1,35 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { LayoutMeasurements, LayoutMeasurementsParams } from "../types/layout/dom"
 import { calculateSectionCount } from "../calculations/layout/section-count"
 import { calculateCellSize } from "../calculations/layout/cell-size";
 import { calculateContainerSize } from "../calculations/layout/container-size";
 
 export function useLayoutMeasurements({ layoutRef, gridDimensions, sections, padding }: LayoutMeasurementsParams) : LayoutMeasurements {
+    const measurementsRef = useRef<LayoutMeasurements>({
+        sectionCount: {
+            horizontal: 0,
+            vertical: 0
+        },
+        cellCount: {
+            horizontal: 0,
+            vertical: 0
+        },
+        cellSize: {
+            full: 0,
+            inner: 0,
+            compact: 0
+        },
+        gridSize: {
+            maxWidth: 0,
+            maxHeight: 0
+        },
+        containerSize: {
+            width: 0,
+            height: 0
+        }
+    });
 
-    const sectionCount = calculateSectionCount(sections);
+    const measurements = measurementsRef.current;
 
-    const cellCount = {
+    measurements.sectionCount = calculateSectionCount(sections);
+    measurements.cellCount = {
         horizontal: gridDimensions.columnCount,
         vertical: gridDimensions.rowCount
     };
-
-    const [cellSize, setCellSize] = useState({
-        full: 0,
-        inner: 0,
-        compact: 0
-    });
-
-    const [gridSize, setGridSize] = useState({
-        maxWidth: 0,
-        maxHeight: 0
-    })
-
-    const [containerSize, setContainerSize] = useState({
-        width: 0,
-        height: 0
-    })
-
-    console.log(gridDimensions, cellCount, gridSize);
 
     useEffect(() => {
         const element = layoutRef.current
@@ -38,38 +44,24 @@ export function useLayoutMeasurements({ layoutRef, gridDimensions, sections, pad
         const observer = new ResizeObserver(() => {
             const { clientWidth, clientHeight } = element
 
-            const _cellSize = calculateCellSize(clientWidth, clientHeight, cellCount, padding);
-            setCellSize(prev =>
-                prev.full === _cellSize.full && prev.inner === _cellSize.inner && prev.compact === _cellSize.compact
-                ? prev : _cellSize
-            );
+            const cellSize = calculateCellSize(clientWidth, clientHeight, measurements.cellCount, padding);
+            measurementsRef.current.cellSize = cellSize;
 
-            const _gridSize = {
-                maxWidth: cellCount.horizontal * _cellSize.inner,
-                maxHeight: cellCount.vertical * _cellSize.inner
+            const gridSize = {
+                maxWidth: measurements.cellCount.horizontal * cellSize.inner,
+                maxHeight: measurements.cellCount.vertical * cellSize.inner
             }
-            setGridSize(prev => 
-                prev.maxWidth === _gridSize.maxWidth && prev.maxHeight === _gridSize.maxHeight
-                ? prev : _gridSize
-            );
             
-            const _containerSize = calculateContainerSize(cellCount, _cellSize.full);
-            setContainerSize(
-                prev => prev.width === _containerSize.width && prev.height === _containerSize.height
-                ? prev : _containerSize
-            );
+            measurementsRef.current.gridSize = gridSize;
+            
+            const containerSize = calculateContainerSize(measurements.cellCount, cellSize.full);
+            measurementsRef.current.containerSize = containerSize;
         })
 
         observer.observe(element)
 
         return () => observer.disconnect()
-    }, [cellCount, sectionCount, padding])
+    }, [measurements.cellCount, measurements.sectionCount, padding])
 
-  return {
-    sectionCount,
-    cellCount,
-    cellSize,
-    gridSize,
-    containerSize
-  }
+  return measurements
 }
