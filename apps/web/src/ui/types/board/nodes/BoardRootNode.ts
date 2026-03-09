26832685e758a16c8a7fcf524062cf5321862448
k4yr2 <kyrzkaya@gmail.com>
2026-03-09 16:39:32 +0300

    feat: add optional cardContainer property to BoardRootNode interface

--- apps/web/src/ui/types/board/nodes/BoardRootNode.ts
+++ apps/web/src/ui/types/board/nodes/BoardRootNode.ts
@@ -1,6 +1,8 @@
 import { RegistryBaseNode } from "@/src/ui/types/shared/registry";
+import { BoardCardContainerNode } from "./BoardCardContainerNode";
 
 
 export interface BoardRootNode extends RegistryBaseNode {
     ref: React.RefObject<HTMLDivElement | null>
+    cardContainer?: BoardCardContainerNode
 }
