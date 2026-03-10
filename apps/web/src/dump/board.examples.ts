76bcca5d4dfcc2de87b53f79f3a606eed16b6730
k4yr2 <kyrzkaya@gmail.com>
2026-03-10 16:01:03 +0300

    feat: update sectionExamples to use createDataIdentifier for IDs

--- apps/web/src/dump/board.examples.ts
+++ apps/web/src/dump/board.examples.ts
@@ -1,7 +1,9 @@
+import { createDataIdentifier } from "../core/utils/shared/data";
+
 export const sectionExamples = { 
   two:[
     {
-      id: "first",
+      id: createDataIdentifier(),
       name: "first",
       colIndex: 1,
       rowIndex: 1,
@@ -9,144 +11,12 @@ export const sectionExamples = {
       rowSpan: 4
     },
     {
-      id: "second",
+      id: createDataIdentifier(),
       name: "second",
       colIndex: 1,
       rowIndex: 5,
       colSpan: 18,
       rowSpan: 4
     }
-  ],
-  three:[
-    {
-      id: "first",
-      colIndex: 1,
-      rowIndex: 1,
-      colSpan: 12,
-      rowSpan: 4
-    }, {
-      id: "second",
-      colIndex: 1,
-      rowIndex: 5,
-      colSpan: 12,
-      rowSpan: 4
-    }, {
-      id: "third",
-      colIndex: 13,
-      rowIndex: 1,
-      colSpan: 6,
-      rowSpan: 8
-    }
-  ],
-  three2:[
-    {
-      id: "first",
-      colIndex: 1,
-      rowIndex: 1,
-      colSpan: 15,
-      rowSpan: 4
-    }, {
-      id: "second",
-      colIndex: 1,
-      rowIndex: 5,
-      colSpan: 15,
-      rowSpan: 4
-    }, {
-      id: "third",
-      colIndex: 16,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }
-  ],
-  four:[
-    {
-      id: "first",
-      colIndex: 1,
-      rowIndex: 1,
-      colSpan: 9,
-      rowSpan: 4
-    },
-    {
-      id: "second",
-      colIndex: 10,
-      rowIndex: 1,
-      colSpan: 9,
-      rowSpan: 4
-    },
-    {
-      id: "third",
-      colIndex: 1,
-      rowIndex: 5,
-      colSpan: 9,
-      rowSpan: 4
-    },
-    {
-      id: "fourth",
-      colIndex: 10,
-      rowIndex: 5,
-      colSpan: 9,
-      rowSpan: 4
-    }
-  ],
-  four2:[
-    {
-      id: "first",
-      colIndex: 1,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }, {
-      id: "second",
-      colIndex: 4,
-      rowIndex: 1,
-      colSpan: 12,
-      rowSpan: 4
-    },  {
-      id: "third",
-      colIndex: 4,
-      rowIndex: 5,
-      colSpan: 12,
-      rowSpan: 4
-    }, {
-      id: "fourth",
-      colIndex: 16,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }
-  ],
-  five:[
-    {
-      id: "first",
-      colIndex: 1,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }, {
-      id: "second",
-      colIndex: 4,
-      rowIndex: 1,
-      colSpan: 9,
-      rowSpan: 4
-    },  {
-      id: "third",
-      colIndex: 4,
-      rowIndex: 5,
-      colSpan: 9,
-      rowSpan: 4
-    }, {
-      id: "fourth",
-      colIndex: 13,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }, {
-      id: "fifth",
-      colIndex: 16,
-      rowIndex: 1,
-      colSpan: 3,
-      rowSpan: 8
-    }
-  ],
+  ]
 }
