889ee4fd1c8ed3c1cd822f8bffd60b25f467465b
k4yr2 <kyrzkaya@gmail.com>
2026-03-10 15:24:02 +0300

    feat: add DataContent type to omit id from BaseData interface

--- apps/web/src/core/types/shared/data.ts
+++ apps/web/src/core/types/shared/data.ts
@@ -2,6 +2,8 @@ export interface BaseData {
     id: Identifier;
 }
 
+export type DataContent<T extends BaseData> = Omit<T, "id">;
+
 export type Identifier = ClientId | ServerId;
 
 export type ClientId = string & { __brand: "ClientId" };
