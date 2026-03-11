b9c0a7e3f0aa0f44bd5dd959611a52d730652b2c
k4yr2 <kyrzkaya@gmail.com>
2026-03-11 14:34:33 +0300

    feat: create AuthStore interface with AuthState and AuthActions for authentication management

---
+++ apps/web/src/core/types/auth/auth.store.ts
@@ -0,0 +1,15 @@
+import { ProfileData } from "@/src/core/types/domain/profile/profile.data";
+
+export interface AuthStore extends AuthState, AuthActions {
+
+}
+
+export interface AuthState {
+    accessToken: string | null;
+    profile: ProfileData | null;
+}
+
+export interface AuthActions {
+    setAuth: (accessToken: string, profile: ProfileData) => void;
+    clearAuth: () => void;
+}
