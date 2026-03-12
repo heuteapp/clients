9143762642823c78e7836a508dc5f89f4dd4b5fc
k4yr2 <kyrzkaya@gmail.com>
2026-03-13 01:19:15 +0300

    feat: add request interceptor to include authorization token for workspace API calls

--- apps/web/src/api/server.ts
+++ apps/web/src/api/server.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import { useAuthStore } from "../stores/auth.store";
 
 export const server = axios.create({
     baseURL: "http://localhost:5120",
     headers: {
         "Content-Type": "application/json",
     }
+});
+
+server.interceptors.request.use((config) => {
+    if(config.url?.startsWith("/workspace")) {
+        const accessToken = useAuthStore(state => state.accessToken);
+
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+    }
+
+    return config;
 });
