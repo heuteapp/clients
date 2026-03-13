f132b1058f1019d7ba7fa2e09df0b3ed3f614be6
k4yr2 <kyrzkaya@gmail.com>
2026-03-13 03:38:08 +0300

    feat: enhance BoardEvent interface and add CardCreatedEvent type for event handling

--- apps/web/src/core/types/domain/board/board.event.ts
+++ apps/web/src/core/types/domain/board/board.event.ts
@@ -1,8 +1,17 @@
-export type BoardEvent = {
+import { BoardCardData } from "./board.data";
+
+export interface BoardEvent {
     occurredAt: string;
     type: BoardEventType;
 }
 
 export enum BoardEventType {
     CardCreated = "CardCreated",
+}
+
+//
+
+export interface CardCreatedEvent extends BoardEvent {
+    type: BoardEventType.CardCreated;
+    data: BoardCardData;
 }
