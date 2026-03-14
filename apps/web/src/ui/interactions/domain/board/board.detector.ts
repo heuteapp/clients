29f37626678460ad4702376ebfa0d47ac79ef3c1
k4yr2 <kyrzkaya@gmail.com>
2026-03-14 04:09:43 +0300

    feat: implement findCardUnderPointer function to detect cards under pointer

---
+++ apps/web/src/ui/interactions/domain/board/board.detector.ts
@@ -0,0 +1,26 @@
+import { Pointer } from "@/src/core/types/shared/common"
+import { BoardRegistry } from "@/src/ui/registries/board.registry.types";
+
+export function findCardUnderPointer(
+    registry: BoardRegistry,
+    pointer: Pointer
+) {
+    for (const card of registry.getBoardCards() ?? []) {
+        const el = card.ref?.current
+        if (!el) continue
+
+        const rect = el.getBoundingClientRect()
+
+        const inside =
+            pointer.x >= rect.left &&
+            pointer.x <= rect.right &&
+            pointer.y >= rect.top &&
+            pointer.y <= rect.bottom
+
+        if (inside) {
+            return { card, rect }
+        }
+    }
+
+    return null
+}
