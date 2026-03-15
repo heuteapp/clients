e39b3cc9bf782e6b02ae51ebb345b0c296c2763a
k4yr2 <kyrzkaya@gmail.com>
2026-03-15 14:00:46 +0300

    refactor: simplify LayoutMetricsValue by removing unused types

--- apps/web/src/core/types/domain/layout/layout.metrics.ts
+++ apps/web/src/core/types/domain/layout/layout.metrics.ts
@@ -1,75 +1,9 @@
 export type LayoutMetricsValue = {
-    spacing: LayoutMetricsSpacing;
-    sectionValue: LayoutSectionMetricsValue;
-    sectionCount: LayoutSectionMetricsCount;
-    sectionContainerValue: LayoutSectionContainerMetricsValue;
+    gridCellSize: LayoutMetricsGridCellSize;
 }
 
-export type LayoutMetricsSpacing = {
-    padding: number;
-}
-
-export type LayoutSectionMetricsValue = {
-    spacing: LayoutSectionMetricsSpacing;
-    gridValue: LayoutGridMetricsValue;
-    size: LayoutSectionMetricsSize;
-}
-
-export type LayoutSectionMetricsSpacing = {
-    padding: number;
-}
-
-export type LayoutSectionMetricsSize = {
-    width: number;
-    height: number;
-}
-
-export type LayoutSectionMetricsCount = {
-    horizontal: number;
-    vertical: number;
-}
-
-export type LayoutSectionContainerMetricsValue = {
-    spacing: LayoutSectionContainerMetricsSpacing;
-    size: LayoutSectionContainerMetricsSize;
-}
-
-export type LayoutSectionContainerMetricsSpacing = {
-    padding: number;
-}
-
-export type LayoutSectionContainerMetricsSize = {
-    width: number;
-    height: number;
-}
-
-export type LayoutGridMetricsValue = {
-    spacing: LayoutGridMetricsSpacing;
-    size: LayoutGridMetricsSize;
-    cellValue: LayoutGridCellMetricsValue;
-    cellCount: LayoutGridCellMetricsCount;
-}
-
-export type LayoutGridMetricsSpacing = {
-    padding: number;
-}
-
-export type LayoutGridMetricsSize = {
-    width: number;
-    height: number;
-}
-
-export type LayoutGridCellMetricsValue = {
-    size: LayoutGridCellMetricsSize;
-}
-
-export type LayoutGridCellMetricsCount = {
-    horizontal: number;
-    vertical: number;
-}
-
-export type LayoutGridCellMetricsSize = {
-    full: number;
+export type LayoutMetricsGridCellSize = {
+    total: number;
     inner: number;
     compact: number;
 }
