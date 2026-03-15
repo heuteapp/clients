59129de95a567ec705e7a87ca951ea4b6042645b
k4yr2 <kyrzkaya@gmail.com>
2026-03-15 15:23:34 +0300

    refactor: simplify BoardState type by using BoardContentValue and removing unused imports

--- apps/web/src/core/types/domain/board/board.store.ts
+++ apps/web/src/core/types/domain/board/board.store.ts
@@ -1,17 +1,12 @@
-import { BoardData, BoardCardData } from "@/src/core/types/domain/board/board.data"
-import { HeuteLayoutData, LayoutSectionData } from "@/src/core/types/domain/layout/layout.data"
-import { DataContent, Identifier } from "@/src/core/types/shared/data"
+import { BoardCardData } from "@/src/core/types/domain/board/board.data"
+import { DataContent } from "@/src/core/types/shared/data"
+import { BoardContentValue } from "./board.content";
 
 export type BoardStore = BoardState & {
     setState: (state: BoardState) => void
 } & BoardActions;
 
-export type BoardState = {
-    board: BoardData | null
-    cards: BoardCardData[]    
-    layout: HeuteLayoutData | null
-    sections: LayoutSectionData[]
-}
+export type BoardState = BoardContentValue
 
 export type BoardActions = {
     createCard: (content: DataContent<BoardCardData>) => BoardCardData
