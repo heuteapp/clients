20b301ba72c70435f2f3a93c233862a958326565
k4yr2 <kyrzkaya@gmail.com>
2026-03-16 20:05:51 +0300

    feat: enhance AppTheme with dark mode palette and input customizations

--- apps/web/src/ui/themes/mui/AppTheme.tsx
+++ apps/web/src/ui/themes/mui/AppTheme.tsx
@@ -3,11 +3,19 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { AppThemeProps } from '@/src/ui/types/theme/mui/mui.props';
+import { inputsCustomizations } from '@/src/ui/themes/mui/customizations/inputs';
 
 export default function AppTheme(props: AppThemeProps) {
   const { children, themeComponents } = props;
   const theme = React.useMemo(() => {
-    return createTheme({});
+    return createTheme({
+      palette: {
+        mode: 'dark',
+      },
+      components: {
+        ...inputsCustomizations,
+      }
+    });
   }, [themeComponents]);
 
   return (
