70363252f0d223a68dfa45ee36ee92c81bef52e2
k4yr2 <kyrzkaya@gmail.com>
2026-03-16 11:47:05 +0300

    fix: change logout button event handler from onPointerUp to onPointerDown in Sidebar component

--- apps/web/src/ui/components/main/Sidebar.tsx
+++ apps/web/src/ui/components/main/Sidebar.tsx
@@ -57,7 +57,7 @@ function Sidebar() {
             border: "none",
             borderRadius: "4px",
           }}
-          onPointerUp={() => {
+          onPointerDown={() => {
             logout();
           }}
         >
