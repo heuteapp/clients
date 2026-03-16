854fd19978080cea8ee632f81451b8578755e7c4
k4yr2 <kyrzkaya@gmail.com>
2026-03-17 00:59:21 +0300

    feat: refactor authentication management by removing AuthStoreController and implementing useAuthManager hook

--- apps/web/src/ui/types/auth/auth.manager.ts
+++ apps/web/src/ui/types/auth/auth.manager.ts
@@ -1,9 +1,9 @@
 import { SignInRequest } from "@/src/api/models/auth.request";
-import { AuthStoreController } from "./auth.store";
 
 export type AuthManager = {
-    store: AuthStoreController;
     signIn: (request: SignInRequest) => Promise<void>;
     signOut: () => Promise<void>;
     hydrate: () => Promise<void>;
 }
+
+export type AuthManagerRef = React.RefObject<AuthManager | null>;
--- apps/web/src/ui/types/auth/auth.store.ts
+++
@@ -1,6 +0,0 @@
-import { AuthActions, AuthState } from "@/src/core/types/auth/auth.store";
-import { RefObject } from "react";
-
-export type AuthStoreController = RefObject<AuthStoreContent | null>;
-
-export type AuthStoreContent = AuthState & AuthActions;
