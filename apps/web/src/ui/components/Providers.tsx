fd54b74ac2d220082083ed5a23f8feddfed902c9
k4yr2 <kyrzkaya@gmail.com>
2026-03-17 14:40:05 +0300

    feat: add Analytics component to Providers for enhanced tracking

--- apps/web/src/ui/components/Providers.tsx
+++ apps/web/src/ui/components/Providers.tsx
@@ -1,12 +1,16 @@
 import { AppTheme } from "@/src/ui/themes/mui/AppTheme";
 import { AuthProvider } from "./AuthProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AppTheme>
-    <AuthProvider>
-        {children}
-    </AuthProvider>
-    </AppTheme>
+    <>
+      <AppTheme>
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+      </AppTheme>
+      <Analytics />
+    </>
   );
 }
