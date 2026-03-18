bf8690b3e84cbe7ac175d2c6fad3e5d96c59856e
k4yr2 <kyrzkaya@gmail.com>
2026-03-18 17:59:00 +0300

    refactor: enhance BoardBaseInteraction and BoardBaseInteractionCallbacks interfaces for better type management

--- apps/web/src/core/types/domain/board/board.interaction.ts
+++ apps/web/src/core/types/domain/board/board.interaction.ts
@@ -1,4 +1,25 @@
-import { BoardBaseState } from "./board.session";
+import { Pointer } from "@/src/core/types/shared/common";
+import { BoardBaseState, BoardSessionManager } from "./board.session";
+
+export interface BoardBaseInteraction<
+    TInteractionType, 
+    TInteractionCallbacks extends BoardBaseInteractionCallbacks<TInteractionType>> {
+        
+    pointer: Pointer | null;
+    type: TInteractionType;
+    callbacks: TInteractionCallbacks | null;
+    session: BoardSessionManager;
+
+    setCallbacks: (callbacks: TInteractionCallbacks | null) => void;
+}
+
+export interface BoardBaseInteractionCallbacks<TInteractionType> {
+    OnStart?: (type: TInteractionType, state: BoardBaseState) => void;
+    OnUpdate?: (type: TInteractionType, state: BoardBaseState) => void;
+    OnFinish?: (type: TInteractionType, state: BoardBaseState) => void;
+}
+
+//
 
 export enum BoardUserInteractionType {
     Idle = "idle",
@@ -7,8 +28,6 @@ export enum BoardUserInteractionType {
     CardResize = "resize",
 }
 
-export interface BoardUserInteractionCallbacks {
-    OnStart?: (type: BoardUserInteractionType, state: BoardBaseState) => void;
-    OnUpdate?: (type: BoardUserInteractionType, state: BoardBaseState) => void;
-    OnFinish?: (type: BoardUserInteractionType, state: BoardBaseState) => void;
+export interface BoardUserInteractionCallbacks extends BoardBaseInteractionCallbacks<BoardUserInteractionType> {
+
 }
