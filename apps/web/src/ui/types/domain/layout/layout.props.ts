93df334abee8f9b691a9383d8765a162382b0717
k4yr2 <kyrzkaya@gmail.com>
2026-03-18 17:25:31 +0300

    feat: add layout props interfaces for LayoutRoot, LayoutSection, and LayoutGrid

---
+++ apps/web/src/ui/types/domain/layout/layout.props.ts
@@ -0,0 +1,16 @@
+import { LayoutData, LayoutSectionData } from "@/src/core/types/domain/layout/layout.data";
+import { Identifier } from "@/src/core/types/shared/data";
+
+export interface LayoutRootProps extends LayoutData {
+
+}
+
+export interface LayoutSectionProps extends LayoutSectionData {
+
+}
+
+export interface LayoutGridProps  {
+    sectionId: Identifier,
+    colSpan: number,
+    rowSpan: number,
+}
