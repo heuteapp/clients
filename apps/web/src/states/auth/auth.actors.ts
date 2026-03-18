cfa56b1cedd1199e44f373dcfbf777286bc31b80
k4yr2 <kyrzkaya@gmail.com>
2026-03-19 01:02:35 +0300

    feat: add signUp actor for authentication and improve error handling in signIn actor

--- apps/web/src/states/auth/auth.actors.ts
+++ apps/web/src/states/auth/auth.actors.ts
@@ -1,4 +1,4 @@
-import { SignInRequest } from "@/src/api/models/auth.request";
+import { SignInRequest, SignUpRequest } from "@/src/api/models/auth.request";
 import { server } from "@/src/api/server";
 import { fromPromise } from "xstate";
 
@@ -7,6 +7,15 @@ export const signInActor = fromPromise<unknown, SignInRequest>(async ({ input })
         return await server.auth.signIn(input);
     } 
     catch (err: any) {
-        throw new Error(err?.message || "Unknown error");
+        throw new Error(err?.message || "Unknown error from sign in");
+    }
+});
+
+export const signUpActor = fromPromise<unknown, SignUpRequest>(async ({ input }) => {
+    try {
+        return await server.auth.signUp(input);
+    } 
+    catch (err: any) {
+        throw new Error(err?.message || "Unknown error from sign up");
     }
 });
