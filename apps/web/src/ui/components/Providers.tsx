6104cc648514aa84c753868c6d0718567c123875
k4yr2 <kyrzkaya@gmail.com>
2026-03-19 16:25:04 +0300

    refactor: remove AuthProvider and related hooks for streamlined authentication handling

--- apps/web/src/ui/components/Providers.tsx
+++ apps/web/src/ui/components/Providers.tsx
@@ -1,14 +1,11 @@
 import { AppTheme } from "@/src/ui/themes/mui/AppTheme";
-import { AuthProvider } from "./AuthProvider";
 import { Analytics } from "@vercel/analytics/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AppTheme>
-        <AuthProvider>
             {children}
-        </AuthProvider>
       </AppTheme>
       <Analytics />
     </>
