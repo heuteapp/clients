1ea7f03d1a69a2734ed4ec93cfeb1a8e1f45ce5e
k4yr2 <kyrzkaya@gmail.com>
2026-03-20 22:48:31 +0300

    refactor: replace auth.store with authMachine integration and create authService

--- apps/web/src/types/states/auth/auth.store.ts
+++
@@ -1,6 +0,0 @@
-import { ActorRefFrom } from "xstate";
-import { authMachine } from "@/src/states/auth/auth.machine";
-
-export interface AuthStore {
-    service: ActorRefFrom<typeof authMachine>
-}
