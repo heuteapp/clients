28df69fab27b6fc4ba2607292f910808a7c58ed5
k4yr2 <kyrzkaya@gmail.com>
2026-03-20 04:14:56 +0300

    feat: add action event types for authentication flow

---
+++ apps/web/src/types/states/auth/action.events.ts
@@ -0,0 +1,32 @@
+import { ProfileData } from "@/src/core/types/domain/profile/profile.data";
+
+//
+export type HYDRATE_EVENT = { type: "HYDRATE" };
+
+//
+
+export type SIGN_IN_EVENT = { type: "SIGN_IN"; identifier: string; password: string };
+
+export type SIGN_IN_SUCCESS_EVENT = { type: "SIGN_IN_SUCCESS"; accessToken: string; profile: ProfileData };
+
+export type SIGN_IN_FAILURE_EVENT = { type: "SIGN_IN_FAILURE"; error: string };
+
+export type SIGN_IN_EVENTS = SIGN_IN_EVENT | SIGN_IN_SUCCESS_EVENT | SIGN_IN_FAILURE_EVENT;
+
+//
+
+export type SIGN_UP_EVENT = { type: "SIGN_UP"; username: string; email: string; password: string };
+
+export type SIGN_UP_COMPLETED_EVENT = { type: "SIGN_UP_COMPLETED"; accessToken: string; profile: ProfileData };
+
+export type SIGN_UP_EXPIRED_EVENT = { type: "SIGN_UP_EXPIRED" };
+
+export type SIGN_UP_EVENTS = SIGN_UP_EVENT | SIGN_UP_COMPLETED_EVENT | SIGN_UP_EXPIRED_EVENT;
+
+//
+
+export type SIGN_OUT_EVENT = { type: "SIGN_OUT" };
+
+//
+
+export type DONE_INVOKE_HYDRATE_EVENT = { type: "done.invoke.hydrate"; output: { accessToken: string; profile: ProfileData } };
