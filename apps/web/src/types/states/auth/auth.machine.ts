fb4d31c4978f55505e770e538809c4fa002d9724
k4yr2 <kyrzkaya@gmail.com>
2026-03-21 14:42:52 +0300

    refactor: enhance AuthMachine types with ActorRef and state management

--- apps/web/src/types/states/auth/auth.machine.ts
+++ apps/web/src/types/states/auth/auth.machine.ts
@@ -1,5 +1,7 @@
 import { AuthData } from "@/src/types/core/auth/auth.data";
 import { DONE_ACTOR_CHECK_AUTH_EVENT, DONE_ACTOR_CHECK_REGISTRATION_EVENT, HYDRATE_EVENT, SIGN_IN_ALL_EVENTS, SIGN_OUT_EVENT, SIGN_UP_ALL_EVENTS } from "./auth.events";
+import { ActorRefFrom } from "xstate";
+import { authMachine } from "@/src/states/auth/auth.machine";
 
 export interface AuthMachineContext {
   auth: AuthData | null;
@@ -19,3 +21,11 @@ export interface AuthRegistration {
     email: string;
     expiredAt: number;
 }
+
+//
+
+export type AuthMachineActor = ActorRefFrom<typeof authMachine>;
+
+export type AuthMachineState = AuthMachineActor["getSnapshot"] extends () => infer R ? R : never;
+    
+export type AuthMachineSend = AuthMachineActor["send"];
