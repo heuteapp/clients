b1e13ec09e0616b3e80e3af4ad51c9a38c0bbf6e
k4yr2 <kyrzkaya@gmail.com>
2026-03-21 17:25:26 +0300

    refactor: simplify layout structure by removing unnecessary components

--- apps/web/app/workspace/(auth)/(sign)/layout.tsx
+++ apps/web/app/workspace/(auth)/(sign)/layout.tsx
@@ -3,7 +3,6 @@
 import { isSignBusy } from "@/src/states/auth/auth.machine";
 import { useAuthContext } from "@/src/ui/hooks/states/auth/useAuthContext";
 import { CircularProgress } from "@mui/material";
-import { Stack } from "@mui/system";
 
 export default function HomeLayout({
   children,
@@ -15,13 +14,8 @@ export default function HomeLayout({
     }
 
     return (
-        <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            sx={{ minHeight: "100%", width: "100%", backgroundColor: "background.default" }}
-        >
+        <>
             {children}
-        </Stack>
+        </>
     )
 }
---
+++ apps/web/app/workspace/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Stack } from "@mui/material";
+
+export default function HomeLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <Stack direction="column" justifyContent="center" alignItems="center" sx={{ minHeight: "100%", width: "100%", backgroundColor: "background.default" }}>
+      {children}
+    </Stack>
+  )
+}
