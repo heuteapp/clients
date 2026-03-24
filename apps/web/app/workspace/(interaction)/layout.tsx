384f5dbb7855612fdfbc13143bc45442e8427546
k4yr2 <kyrzkaya@gmail.com>
2026-03-25 01:07:44 +0300

    feat: implement WorkspaceLayout with navigation and create new WorkspacePage component

--- apps/web/app/workspace/(interaction)/layout.tsx
+++ apps/web/app/workspace/(interaction)/layout.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useRef } from "react";
-import Monitor from "@/src/ui/components/workspace/Monitor";
+import Box from "@mui/material/Box";
+import { BrandIconOnly } from "@/src/ui/components/app/Brand";
 
 export default function WorkspaceLayout({
   children,
@@ -9,17 +10,35 @@ export default function WorkspaceLayout({
   const rootRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <div ref={rootRef} style= {{
-      display: "flex",
-      height: "100%",
-      width: "100%",
-      overflow: "hidden",
-      touchAction: "none",
-      userSelect: "none",
-    }}>
-        <Monitor>
-            {children}
-        </Monitor>
-    </div>
+    <Box>
+      <Box
+        component="nav"
+        sx={{
+          borderBottom: "1px solid #7f7f7f53",
+          width: "100%",
+          height: "48px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          color: "#FFF",
+          fontSize: "1.5rem",
+        }}
+      >
+        <BrandIconOnly 
+          link={{ href: "/", linkType: "external" }}
+          size={28} 
+          style={{ padding: 8 }}
+        />
+        <div
+          style={{
+            fontSize: "1.125rem",
+            color: "#6c6c6c6d",
+            fontWeight: "600",
+          }}
+        >
+          /
+        </div>
+      </Box>
+    </Box>
   )
 }
