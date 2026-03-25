12e3643c18a6219fcce2ac73f802a094fe096dcb
k4yr2 <kyrzkaya@gmail.com>
2026-03-25 22:00:43 +0300

    feat(board): add custom hook useBoardContext for accessing BoardContext

---
+++ apps/web/src/ui-board/hooks/useBoardContext.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { BoardContext } from "@/src/ui-board/contexts/board.context";
+
+export const useBoardContext = () => {
+    const context = useContext(BoardContext);
+    
+    if (!context) {
+        throw new Error("useBoardContext must be used within an BoardProvider");
+    }
+
+    return context;
+};
