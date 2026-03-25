73891a2a6f6e9afaf61b67c13852b62b465bba24
k4yr2 <kyrzkaya@gmail.com>
2026-03-26 00:08:23 +0300

    feat(types): migrate UIEntity and UINode interfaces to ui-base and remove obsolete ui-shared types

--- apps/web/src/ui-board/types/board.entity.ts
+++ apps/web/src/ui-board/types/board.entity.ts
@@ -1,4 +1,4 @@
-import { UIEntity } from "@/src/ui-shared/types/ui.types";
+import { UIEntity } from "@/src/ui-base/types/ui.types";
 import { BoardCardData, BoardData } from "@/src/board/types/board.types";
 
 /**
