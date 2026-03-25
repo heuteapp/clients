dd9ac2320910bad68e6662877e9a720eec8042c3
k4yr2 <kyrzkaya@gmail.com>
2026-03-25 04:16:31 +0300

    fix: refactor layout structure for improved responsiveness and organization

--- apps/web/app/workspace/(interaction)/layout.tsx
+++ apps/web/app/workspace/(interaction)/layout.tsx
@@ -3,6 +3,7 @@
 import { useRef } from "react";
 import Box from "@mui/material/Box";
 import { BrandIconOnly } from "@/src/ui/components/app/Brand";
+import { flex } from "@mui/system";
 
 export default function WorkspaceLayout({
   children,
@@ -49,15 +50,45 @@ export default function WorkspaceLayout({
       <Box
         sx={{
           width: "100%",
+          height: "100%",
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          py: 4,
+          flexDirection: "row",
           bgcolor: "transparent"
         }}
       >
-      {children}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100vh",
+            width: 400,
+            backgroundColor: "green",
+          }}
+        >
+          {/* do 5 96x96 items */}
+          {[...Array(5)].map((_, index) => (
+            <Box
+              key={index}
+              sx={{
+                width: 48,
+                height: 48,
+                backgroundColor: "lightgray",
+                margin: 2,
+              }}
+            />
+          ))}
+        </Box>        
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {children}
+        </Box>
       </Box>
     </Box>
   )
