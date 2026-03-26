8aa4288941a34d8ccf82cde8009cc54d1c00fe8c
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 02:50:19 +0300

    refactor(workspace-breadcrumbs): replace HeuteIconOnlyBrand with custom root item for improved structure

--- apps/web/src/modules/workspace/components/WorkspaceBreadcrumbs.tsx
+++ apps/web/src/modules/workspace/components/WorkspaceBreadcrumbs.tsx
@@ -1,31 +1,41 @@
+import { Box } from "@mui/material";
 import { HeuteIconOnlyBrand } from "../../ui-shared/components/HeuteBrand";
 import { HeuteLinkedBreadcrumbs } from "../../ui-shared/components/HeuteBreadcrumbs";
 import { useWorkspaceContext } from "../hooks/useWorkspaceContext";
+import { BrandIcon } from "../../ui-base/components/Brand";
 
 export function WorkspaceBreadcrumbs() {
     const context = useWorkspaceContext();
     const { segmentsResult } = context.metadata;
 
-    const items = segmentsResult.segments.map((segment, index) => ({
+    const rootItem = {
+        name: "Home",
+        href: "/workspace",
+        element: () => (
+            <BrandIcon 
+                size={24}
+                sx={{
+                    p: 1
+                }}
+            />
+        )
+    }
+
+    const segmentItems = segmentsResult.segments.map((segment, index) => ({
         name: segment,
         href: segmentsResult.fullPath + "?display=" + index,
     }));
 
+    const items = [rootItem, ...segmentItems];
+
     return (
         <>
-            <HeuteIconOnlyBrand 
-                link={{ href:"/", linkType: "external" }} 
-                iconSize={28} 
-                iconProps={{
-                    sx: {
-                        padding: 1
-                    }
-                }}
-            />
             <HeuteLinkedBreadcrumbs 
                 linkProps={{
                     sx: {
-                        textDecoration: "none"
+                        textDecoration: "none",
+                        display: "flex",
+                        alignItems: "center",
                     }
                 }}
                 items={items}
