876aaa52559f6d1c3db972f5855be7ddb8a76344
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 00:46:33 +0300

    feat(workspace): enhance useWorkspace hook to include segmentsResult in WorkspaceMetadata

--- apps/web/src/modules/workspace/hooks/useWorkspace.ts
+++ apps/web/src/modules/workspace/hooks/useWorkspace.ts
@@ -1,10 +1,13 @@
 import { WorkspaceMetadata } from "@/src/modules/workspace/types/workspace.types";
 import { useWorkspaceType } from "./useWorkspaceType";
+import { useWorkspaceSegments } from "./useWorkspaceSegments";
 
 export const useWorkspace = () : WorkspaceMetadata => {
     const type = useWorkspaceType();
+    const segmentsResult = useWorkspaceSegments();
 
     return {
         type,
+        segmentsResult,
     }
 }
--- apps/web/src/modules/workspace/types/workspace.types.ts
+++ apps/web/src/modules/workspace/types/workspace.types.ts
@@ -1,5 +1,8 @@
+import { PathSegmentsResult } from "../../ui-shared/types/path-segments.types";
+
 export type WorkspaceMetadata = {
     type: WorkspaceType;
+    segmentsResult: PathSegmentsResult;
 }
 
 export type WorkspaceType = "root" | "board" | "unknown";
