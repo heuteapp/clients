014da1f600270190e67c2fe348c3d7b8bb3e694d
k4yr2 <kyrzkaya@gmail.com>
2026-03-26 03:23:50 +0300

    refactor(layout.props): remove unused Identifier import

--- apps/web/src/modules/ui-layout/types/layout.props.ts
+++ apps/web/src/modules/ui-layout/types/layout.props.ts
@@ -1,5 +1,4 @@
 import { LayoutEntity, LayoutSectionEntity } from "@/src/modules/ui-layout/types/layout.entity";
-import { Identifier } from "@/src/types/shared/core/data";
 
 export interface LayoutRootProps extends LayoutEntity {
 
