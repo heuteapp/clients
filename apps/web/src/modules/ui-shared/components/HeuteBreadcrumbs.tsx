e6ac941b00067d5738705be74c284d09d7e105b7
k4yr2 <kyrzkaya@gmail.com>
2026-03-26 23:32:41 +0300

    feat(breadcrumbs): add HeuteLinkedBreadcrumbs component with defaultElement function

---
+++ apps/web/src/modules/ui-shared/components/HeuteBreadcrumbs.tsx
@@ -0,0 +1,22 @@
+import { Breadcrumbs } from "@/src/modules/ui-base/components/Breadcrumbs"
+import { HeuteLinkedBreadcrumbsProps } from "@/src/modules/ui-shared/types/components/heute-breadcrumbs.types"
+import { HeuteLink } from "./HeuteLink"
+
+export const HeuteLinkedBreadcrumbs = (props: HeuteLinkedBreadcrumbsProps) => {
+    const defaultElement = (name: string, href?: string) => {
+        return (
+            <HeuteLink
+                href={href}
+            >
+                {name}
+            </HeuteLink>
+        )
+    }
+
+    return (
+        <Breadcrumbs
+            defaultElement={defaultElement}
+            {...props}
+        />
+    )
+}
