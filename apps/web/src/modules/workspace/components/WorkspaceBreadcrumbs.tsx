70973654882b58da9aac46f80ed058d8e1b28185
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 01:30:29 +0300

    feat(workspace-breadcrumbs): refactor WorkspaceBreadcrumbs to include HeuteIconOnlyBrand and improve structure

--- apps/web/src/modules/workspace/components/WorkspaceBreadcrumbs.tsx
+++ apps/web/src/modules/workspace/components/WorkspaceBreadcrumbs.tsx
@@ -1,5 +1,6 @@
 import { BrandIcon } from "../../ui-base/components/Brand";
 import { BreadcrumbsItem } from "../../ui-base/types/breadcrumbs.types";
+import { HeuteIconOnlyBrand } from "../../ui-shared/components/HeuteBrand";
 import { HeuteLinkedBreadcrumbs } from "../../ui-shared/components/HeuteBreadcrumbs";
 import { useWorkspaceContext } from "../hooks/useWorkspaceContext";
 
@@ -7,20 +8,29 @@ export function WorkspaceBreadcrumbs() {
     const context = useWorkspaceContext();
     const { segmentsResult } = context.metadata;
 
-    const rootItem : BreadcrumbsItem = {
-        name: null!,
-        href: "/workspace",
-        element: () => <BrandIcon/>
-    };
-    
     const items = segmentsResult.segments.map((segment, index) => ({
         name: segment,
         href: segmentsResult.fullPath + "?display=" + index,
     }));
 
     return (
-        <HeuteLinkedBreadcrumbs
-            items={[rootItem, ...items]}
-        />
+        <>
+            <HeuteIconOnlyBrand 
+                size={28} 
+                link={{ href:"/", linkType: "external" }} 
+                style={{
+                    padding: 8
+                }}
+            />
+            <HeuteLinkedBreadcrumbs 
+                linkProps={{
+                    style: {
+                        textDecoration: "none",
+                        color: "white"
+                    }
+                }}
+                items={items}
+            />
+        </>
     )
 }
