21cfa05c97d548343f5634abd24e3999247ffbfb
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 03:48:16 +0300

    feat(metadata): update app metadata for improved clarity and functionality

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,10 +6,20 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HeuteApp",
-  description: "Daily learning journey",
+  title: 'HeuteApp',
+  description: 'Daily what you learned, Yours to keep.',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'HeuteApp',
+  },
   icons: {
-    icon: "/favicon-32x32.png",
+    apple: '/icons/apple-icon-180.png',
+    icon: [
+      { url: '/icons/manifest-icon-192.maskable.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/manifest-icon-512.maskable.png', sizes: '512x512', type: 'image/png' },
+    ],
   },
 };
 
@@ -20,6 +30,9 @@ export default function RootLayout({
     <html lang="en" className={`${inter.className}`}>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body>
         <AppProviders>
