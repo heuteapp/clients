9cbdc25bcff7555149e078f7d29980921fdcedbf
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 03:12:23 +0300

    feat(layout): add manifest link to RootLayout and create manifest.json for PWA support

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${inter.className}`}>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body>
         <AppProviders>
           {children}
