2754a10885771f290b9886f83c5b3522fd08ccab
k4yr2 <kyrzkaya@gmail.com>
2026-03-27 04:45:08 +0300

    feat(AppProviders): integrate PWA guard for enhanced workspace management

--- apps/web/app/AppProviders.tsx
+++ apps/web/app/AppProviders.tsx
@@ -2,8 +2,11 @@ import { AppTheme } from "@/src/modules/ui-shared/themes/mui/AppTheme";
 import { Analytics } from "@vercel/analytics/next";
 import { AuthProvider } from "@/src/modules/ui-auth/providers/AuthProvider";
 import { Suspense } from "react";
+import { usePWAGuard } from "@/src/modules/ui-shared/hooks/usePWAGuard";
 
 export function AppProviders({ children } : { children: React.ReactNode }) {
+  usePWAGuard();
+
   return (
     <>
       <AppTheme>
