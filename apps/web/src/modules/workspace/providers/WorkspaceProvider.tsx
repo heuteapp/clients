29a5fa4a71806aba960da86ce8d95ab00092afb4
k4yr2 <kyrzkaya@gmail.com>
2026-03-28 20:30:18 +0300

    refactor(workspace): update WorkspaceProvider to include useWorkspaceCache and improve context value

--- apps/web/src/modules/workspace/providers/WorkspaceProvider.tsx
+++ apps/web/src/modules/workspace/providers/WorkspaceProvider.tsx
@@ -4,20 +4,20 @@ import React from "react";
 import { WorkspaceContext } from "@/src/modules/workspace/contexts/workspace.context";
 import { useWorkspaceMetadata } from "@/src/modules/workspace/hooks/useWorkspaceMetadata";
 import { useWorkspaceBreadcrumbs } from "../hooks/useWorspaceBreadcrumbs";
+import { useWorkspaceCache } from "../hooks/useWorkspaceCache";
 
 export function WorkspaceProvider({ children }: { children: React.ReactNode }) {
     const metadata = useWorkspaceMetadata();
     const breadcrumbs = useWorkspaceBreadcrumbs();
+    const cache = useWorkspaceCache();
     
     const contextValue = React.useMemo(() => {
-        return { metadata, breadcrumbs, cache: null! };
-    }, [metadata]);
+        return { metadata, breadcrumbs, cache };
+    }, [metadata, breadcrumbs, cache]);
 
     return (
-        <>
-            <WorkspaceContext.Provider value={contextValue}>
-                {children}
-            </WorkspaceContext.Provider>
-        </>
+        <WorkspaceContext.Provider value={contextValue}>
+            {children}
+        </WorkspaceContext.Provider>
     )
 }
