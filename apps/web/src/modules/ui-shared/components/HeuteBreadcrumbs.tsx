2072fa65b2c9b039deae50d083b24b287a479548
k4yr2 <kyrzkaya@gmail.com>
2026-03-28 14:59:10 +0300

    feat(breadcrumbs): enhance HeuteAnimatedBreadcrumbs to support items and improve animation rendering

--- apps/web/src/modules/ui-shared/components/HeuteBreadcrumbs.tsx
+++ apps/web/src/modules/ui-shared/components/HeuteBreadcrumbs.tsx
@@ -1,7 +1,8 @@
 import { Breadcrumbs } from "@/src/modules/ui-base/components/Breadcrumbs"
-import { HeuteAnimatedBreadcrumbsProps, HeuteLinkedBreadcrumbsProps } from "@/src/modules/ui-shared/types/components/heute-breadcrumbs.types"
+import { HeuteAnimatedBreadcrumbsItem, HeuteAnimatedBreadcrumbsProps, HeuteLinkedBreadcrumbsProps } from "@/src/modules/ui-shared/types/components/heute-breadcrumbs.types"
 import { BreadcrumbsItemData } from "@/src/modules/ui-base/types/breadcrumbs.types"
 import { HeuteLink } from "./HeuteLink"
+import { AnimatePresence, motion } from "framer-motion"
 
 export const HeuteLinkedBreadcrumbs = ({ linkProps, renderItem, ...props }: HeuteLinkedBreadcrumbsProps) => {
     const linkRender = (item: BreadcrumbsItemData, index?: number) => {
@@ -27,12 +28,36 @@ export const HeuteLinkedBreadcrumbs = ({ linkProps, renderItem, ...props }: Heut
     )
 }
 
-export const HeuteAnimatedBreadcrumbs = ({ delay, offset, animate = true, renderItem, separator, ...props }: HeuteAnimatedBreadcrumbsProps) => {
+export const HeuteAnimatedBreadcrumbs = ({ items, renderItem, delay = 0, offset, animate = true, separator, ...props }: HeuteAnimatedBreadcrumbsProps) => {
+    const animationRender = (item: HeuteAnimatedBreadcrumbsItem, index?: number) => {
+        const content = renderItem 
+            ? renderItem(item, index)
+            : item.name;
+
+        if (!animate || item.animate === false) {
+            return <>{content}</>;
+        }
+
+        return (
+            <motion.div
+                initial={{ opacity: 0, x: -10 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                    duration: delay,
+                    delay: (delay),
+                    type: "tween",
+                    damping: 12,
+                }}
+            >
+                {content}
+            </motion.div>
+        )
+    }
 
     return (
         <Breadcrumbs 
-            /*renderItem={animationRender}
-            separator={animationSeparator}*/
+            items={items}
+            renderItem={animationRender}
             {...props}
         />
     )
