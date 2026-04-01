21d2418996ed3ef74a1e87a8653593b62b614be6
k4yr2 <kyrzkaya@gmail.com>
2026-04-02 00:08:21 +0300

    refactor(AppProviders): remove QueryClientProvider and simplify structure refactor(WorkspaceProvider): clean up imports and utilize useCategoriesLoader

--- apps/web/app/AppProviders.tsx
+++ apps/web/app/AppProviders.tsx
@@ -3,23 +3,19 @@
 import { AppTheme } from "@/src/modules/ui-shared/themes/mui/AppTheme";
 import { Analytics } from "@vercel/analytics/next";
 import { AuthProvider } from "@/src/modules/ui-auth/providers/AuthProvider";
-import { Suspense, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 export function AppProviders({ children } : { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <AppTheme>
-          <Suspense fallback={null}>
-            <AuthProvider>
-              {children}
-            </AuthProvider>
-          </Suspense>
-        </AppTheme>
-      </QueryClientProvider>
+      <AppTheme>
+        <Suspense fallback={null}>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </Suspense>
+      </AppTheme>
       <Analytics />
     </>
   );
--- apps/web/src/modules/workspace/providers/WorkspaceProvider.tsx
+++ apps/web/src/modules/workspace/providers/WorkspaceProvider.tsx
@@ -1,36 +1,19 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { WorkspaceContext } from "@/src/modules/workspace/contexts/workspace.context";
 import { useWorkspaceMetadata } from "@/src/modules/workspace/hooks/useWorkspaceMetadata";
 import { useWorkspaceBreadcrumbs } from "../hooks/useWorkspaceBreadcrumbs";
 import { useWorkspaceCache } from "../hooks/useWorkspaceCache";
-
-import { useQuery } from "@tanstack/react-query";
-import { heuteApi } from "@/src/api/heuteApi";
-import { useCategoryStore } from "@/src/heute-store/stores/category.store";
-import { useAuthContext } from "../../ui-auth/hooks/useAuthContext";
+import { useCategoriesLoader } from "../../ui-category/hooks/useCategoriesLoader";
 
 export function WorkspaceProvider({ children }: { children: React.ReactNode }) {
-    const { state } = useAuthContext();
     const metadata = useWorkspaceMetadata();
     const breadcrumbs = useWorkspaceBreadcrumbs();
     const cache = useWorkspaceCache();
-    
-    const { data: categories } = useQuery({
-        queryKey: ["categories"],
-        queryFn: () => heuteApi.me.categories.getHierarchy(),
-        enabled: state.matches("authenticated")
-    });
 
-    const { loadMe } = useCategoryStore();
+    useCategoriesLoader();
 
-    useEffect(() => {
-        if (categories) {
-            loadMe(categories);
-        }
-    }, [categories, loadMe]);
-    
     const contextValue = React.useMemo(() => {
         return { metadata, breadcrumbs, cache };
     }, [metadata, breadcrumbs, cache]);
