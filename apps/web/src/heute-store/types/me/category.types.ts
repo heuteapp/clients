f2d72c6001dee622adce606c4afb3c56ff9bcb2b
k4yr2 <kyrzkaya@gmail.com>
2026-04-01 16:08:43 +0300

    feat(api): add additional properties to MeCategoryState interface

--- apps/web/src/heute-store/types/me/category.types.ts
+++ apps/web/src/heute-store/types/me/category.types.ts
@@ -3,4 +3,7 @@ import { StoredCategory } from "@/src/heute-store/types/category.types";
 export interface MeCategoryState {
     byId: Map<string, StoredCategory>;
     byParentId: Map<string, string[]>;
+    rootIds: string[];
+    isLoading: boolean;
+    error: string | null;
 }
