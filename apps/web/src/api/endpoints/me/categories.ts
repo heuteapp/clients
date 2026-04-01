38ddd01a392634383eebe1c9fdf7027d9e9bc0c9
k4yr2 <kyrzkaya@gmail.com>
2026-04-01 14:53:52 +0300

    feat(api): specify response types for category endpoint methods

--- apps/web/src/api/endpoints/me/categories.ts
+++ apps/web/src/api/endpoints/me/categories.ts
@@ -1,12 +1,13 @@
 import { heuteClient } from "@/src/api/heuteClient";
+import { CategoryChainResponse, CategoryTreeResponse, CategoryHierarchyResponse } from "@/src/api/models/responses/category.response";
 
 export const categories = {
     getCategoryChain : (path: string) => 
-        heuteClient.get(`/me/categories/chain/${encodeURIComponent(path)}`).then(res => res.data),
+        heuteClient.get<CategoryChainResponse>(`/me/categories/chain/${encodeURIComponent(path)}`).then(res => res.data),
 
     getCategoryTree : (path: string) => 
-        heuteClient.get(`/me/categories/tree/${encodeURIComponent(path)}`).then(res => res.data),
+        heuteClient.get<CategoryTreeResponse>(`/me/categories/tree/${encodeURIComponent(path)}`).then(res => res.data),
 
-    getCategoryHierarchy : (path: string) => 
-        heuteClient.get(`/me/categories/hierarchy/${encodeURIComponent(path)}`).then(res => res.data)
+    getCategoryHierarchy : () => 
+        heuteClient.get<CategoryHierarchyResponse>(`/me/categories/hierarchy`).then(res => res.data)
 };
