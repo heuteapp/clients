fd0815a76a8c7859a9bb7686e5842f14fb68d191
k4yr2 <kyrzkaya@gmail.com>
2026-04-01 14:51:54 +0300

    feat(api): update import path for heuteClient and specify response type for getDailyboard

--- apps/web/src/api/endpoints/me/dailyboards.ts
+++ apps/web/src/api/endpoints/me/dailyboards.ts
@@ -1,6 +1,7 @@
-import { heuteClient } from "../../heuteClient";
+import { heuteClient } from "@/src/api/heuteClient";
+import { DailyboardResponse } from "@/src/api/models/responses/dailyboard.response";
 
 export const dailyboards = {
     getDailyboard : (path: string) => 
-        heuteClient.get(`/me/dailyboards/${encodeURIComponent(path)}`).then(res => res.data)
+        heuteClient.get<DailyboardResponse>(`/me/dailyboards/${encodeURIComponent(path)}`).then(res => res.data)
 };
