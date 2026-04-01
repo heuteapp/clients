958f0a02b60e8b558abf8f6f011aa1d667c3dad1
k4yr2 <kyrzkaya@gmail.com>
2026-04-01 14:21:13 +0300

    feat(auth): implement authEndpoints for sign-in, sign-up, and user profile retrieval

---
+++ apps/web/src/api/endpoints/auth.endpoints.ts
@@ -0,0 +1,15 @@
+import { heuteClient } from "@/src/api/client";
+import { SignInRequest, SignUpRequest } from "@/src/api/models/requests/auth.request";
+import { SignInResponse, SignUpResponse } from "@/src/api/models/responses/auth.response";
+import { AuthProfile } from "@/src/modules/auth/types/auth.types";
+
+export const authEndpoints = {
+    signIn: (request: SignInRequest): Promise<SignInResponse> =>
+        heuteClient.post<SignInResponse>("/auth/sign-in", request).then(res => res.data),
+    
+    signUp: (request: SignUpRequest): Promise<SignUpResponse> =>
+        heuteClient.post<SignUpResponse>("/auth/sign-up", request).then(res => res.data),
+
+    me: () : Promise<AuthProfile | null> =>
+        heuteClient.get("/auth/me").then(res => res.data),
+};
