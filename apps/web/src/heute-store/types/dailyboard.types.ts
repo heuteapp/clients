021927a523645a06194aee1690c49b0b3515ef6e
k4yr2 <kyrzkaya@gmail.com>
2026-04-03 14:58:57 +0300

    fix(dailyboard): update return type of getMeDailyboard and getUserDailyboard to StoredDailyboard for consistency

--- apps/web/src/heute-store/types/dailyboard.types.ts
+++ apps/web/src/heute-store/types/dailyboard.types.ts
@@ -6,12 +6,12 @@ export interface DailyboardState extends UserBasedStoreState<StoredDailyboard> {
     loadMeDailyboard: (dailyboard: Dailyboard) => void;
     loadUserDailyboard: (user: string, dailyboard: Dailyboard) => void;
 
-    getMeDailyboard: (categoryPath: string, date: YYMMDDDate) => Dailyboard | null;
-    getUserDailyboard: (user: string, categoryPath: string, date: YYMMDDDate) => Dailyboard | null;
+    getMeDailyboard: (categoryPath: string, date: YYMMDDDate) => StoredDailyboard | null;
+    getUserDailyboard: (user: string, categoryPath: string, date: YYMMDDDate) => StoredDailyboard | null;
 }
 
 export interface StoredDailyboard extends StoredItem, DailyboardData {
-    
+
 }
 
 export interface StoredDailyboardCard extends StoredItem, DailyboardCardData {
