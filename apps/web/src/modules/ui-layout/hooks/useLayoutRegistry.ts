6c6e379e70feaef89f67cff83d453fd796c48a6c
k4yr2 <kyrzkaya@gmail.com>
2026-04-03 21:51:25 +0300

    fix(layout): correct cleanup function in useLayoutRegistry to properly initialize layout registry

--- apps/web/src/modules/ui-layout/hooks/useLayoutRegistry.ts
+++ apps/web/src/modules/ui-layout/hooks/useLayoutRegistry.ts
@@ -6,9 +6,7 @@ export const useLayoutRegistry = (layoutRef: React.RefObject<HTMLDivElement | nu
     const registry = React.useRef<LayoutRegistry>(null);
 
     React.useEffect(() => {
-        return () => {
-            registry.current = createLayoutRegistry(layoutRef);
-        }
+        registry.current = createLayoutRegistry(layoutRef);
     }, []);
 
     return registry.current!;
