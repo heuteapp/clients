eec5a9a00ae2b658d1bad3358285065404f51dc9
k4yr2 <kyrzkaya@gmail.com>
2026-04-04 23:29:43 +0300

    refactor(layout): enhance LayoutBaseState and StoredLayoutRootItem for improved type safety

--- apps/web/src/heute-store/types/layout/layout.base.types.ts
+++ apps/web/src/heute-store/types/layout/layout.base.types.ts
@@ -1,16 +1,20 @@
 import { LayoutBase, LayoutSectionBase } from "@/src/modules/layout/types/layout.base.types";
 import { StoredItem, UserBasedStoreState } from "../store.types";
 
-export interface LayoutBaseState<TLayout extends StoredLayoutItem> extends UserBasedStoreState<TLayout> {
+export interface LayoutBaseState<
+    TLayout extends StoredLayoutItem,
+    TLayoutRoot extends StoredLayoutRootItem<TLayoutSection>,
+    TLayoutSection extends StoredLayoutSectionItem
+> extends UserBasedStoreState<TLayout> {
     sectionById: Record<string, StoredLayoutSectionItem>;
 
     loadGlobalLayout: (layout: TLayout) => void;
     loadMeLayout: (layout: TLayout) => void;
     loadUserLayout: (user: string, layout: TLayout) => void;
 
-    getGlobalLayout: (name: string, version: number) => StoredLayoutRootItem | null;
-    getMeLayout: (name: string, version: number) => StoredLayoutRootItem | null;
-    getUserLayout: (user: string, name: string, version: number) => StoredLayoutRootItem | null;
+    getGlobalLayout: (name: string, version: number) => TLayoutRoot | null;
+    getMeLayout: (name: string, version: number) => TLayoutRoot | null;
+    getUserLayout: (user: string, name: string, version: number) => TLayoutRoot | null;
 }
 
 //
@@ -19,8 +23,8 @@ export interface StoredLayoutItem extends StoredItem, LayoutBase {
 
 }
 
-export interface StoredLayoutRootItem extends StoredLayoutItem {
-    sections: StoredLayoutSectionItem[];
+export interface StoredLayoutRootItem<TLayoutSection extends StoredLayoutSectionItem> extends StoredLayoutItem {
+    sections: TLayoutSection[];
 }
 
 export interface StoredLayoutSectionItem extends StoredItem, LayoutSectionBase {
