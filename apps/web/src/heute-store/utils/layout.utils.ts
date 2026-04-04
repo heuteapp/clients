939b285d69eec2c7c8076bb342095322ba615f97
k4yr2 <kyrzkaya@gmail.com>
2026-04-05 01:38:40 +0300

    feat(layout): add saveLayoutToState function for managing layout state

--- apps/web/src/heute-store/utils/layout.utils.ts
+++ apps/web/src/heute-store/utils/layout.utils.ts
@@ -1,4 +1,5 @@
-import { LayoutBaseSource, LayoutSectionBaseSource, StoredLayoutItemContent, StoredLayoutSectionItemContent } from "../types/layout/layout.base.types";
+import { LayoutState } from "../types/layout.types";
+import { LayoutBaseSource, LayoutBaseState, LayoutSectionBaseSource, StoredLayoutItem, StoredLayoutItemContent, StoredLayoutSectionItem, StoredLayoutSectionItemContent } from "../types/layout/layout.base.types";
 
 export const convertLayoutSourceToItemContent = <
     TSource extends LayoutBaseSource, 
@@ -22,3 +23,25 @@ export const convertLayoutSectionSourceToItemContent = <
         ...source,
     } as unknown as TItemContent;
 }
+
+//
+
+export const saveLayoutToState = <
+    TLayoutSource extends LayoutBaseSource,
+    TLayoutItem extends StoredLayoutItem<TLayoutSection>,
+    TLayoutItemContent extends StoredLayoutItemContent,
+    TLayoutSection extends StoredLayoutSectionItem,
+    TLayoutSectionContent extends StoredLayoutSectionItemContent
+>(
+    state: LayoutBaseState<TLayoutSource, TLayoutItem, TLayoutItemContent, TLayoutSection, TLayoutSectionContent>, 
+    owner: string, layout: TLayoutSource
+) => {
+    const layoutId = `${owner}@${layout.name}/${layout.version}`;
+
+    state.byId[layoutId] = convertLayoutSourceToItemContent(layoutId, layout);
+
+    layout.sections.forEach((section) => {
+        const sectionId = `${layoutId}/${section.name}`;
+        state.sectionById[sectionId] = convertLayoutSectionSourceToItemContent(sectionId, section);
+    });
+};
