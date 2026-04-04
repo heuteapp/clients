5067cd111ce9c645d2e11bc0330d525c344e1b48
k4yr2 <kyrzkaya@gmail.com>
2026-04-05 01:32:10 +0300

    feat(layout): add sections property to LayoutBase interface for improved structure

--- apps/web/src/modules/layout/types/layout.base.types.ts
+++ apps/web/src/modules/layout/types/layout.base.types.ts
@@ -1,8 +1,15 @@
 export interface LayoutBase {
     name: string;
     version: number;
+    sections: LayoutSectionBase[];
 }
 
 export interface LayoutSectionBase {
     name: string;
 }
+
+//
+
+export type LayoutBaseContent = Omit<LayoutBase, "sections">;
+
+export type LayoutSectionBaseContent = LayoutSectionBase;
