722374a30275178d80baf3d417dba28061c479ca
k4yr2 <kyrzkaya@gmail.com>
2026-04-04 23:55:05 +0300

    feat(layout): add layout style interfaces for improved layout management

---
+++ apps/web/src/heute-store/types/layout/layout.style.types.ts
@@ -0,0 +1,20 @@
+import { LayoutBaseState, StoredLayoutItem, StoredLayoutRootItem, StoredLayoutSectionItem } from "./layout.base.types";
+import { LayoutSectionStyle, LayoutStyle } from "@/src/modules/layout/types/layout.style.types";
+
+export interface LayoutStyleState extends LayoutBaseState<StoredLayoutStyle, StoredLayoutRootStyle, StoredLayoutSectionStyle> {
+
+}
+
+//
+
+export interface StoredLayoutStyle extends StoredLayoutItem, LayoutStyle {
+
+}
+
+export interface StoredLayoutRootStyle extends StoredLayoutStyle, StoredLayoutRootItem<StoredLayoutSectionStyle> {
+
+}
+
+export interface StoredLayoutSectionStyle extends StoredLayoutSectionItem, LayoutSectionStyle {
+    layoutId: () => string;
+}
