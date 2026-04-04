1412c7d06981c978d8b644255c87e8a9df42e59e
k4yr2 <kyrzkaya@gmail.com>
2026-04-05 01:41:49 +0300

    feat(layout): add functions for retrieving layout items and their contents

--- apps/web/src/heute-store/utils/layout.utils.ts
+++ apps/web/src/heute-store/utils/layout.utils.ts
@@ -1,4 +1,3 @@
-import { LayoutState } from "../types/layout.types";
 import { LayoutBaseSource, LayoutBaseState, LayoutSectionBaseSource, StoredLayoutItem, StoredLayoutItemContent, StoredLayoutSectionItem, StoredLayoutSectionItemContent } from "../types/layout/layout.base.types";
 
 export const convertLayoutSourceToItemContent = <
@@ -44,4 +43,55 @@ export const saveLayoutToState = <
         const sectionId = `${layoutId}/${section.name}`;
         state.sectionById[sectionId] = convertLayoutSectionSourceToItemContent(sectionId, section);
     });
+};
+
+export const getLayoutItem = <
+    TLayoutSource extends LayoutBaseSource,
+    TLayoutItem extends StoredLayoutItem<TLayoutSection>,
+    TLayoutItemContent extends StoredLayoutItemContent,
+    TLayoutSection extends StoredLayoutSectionItem,
+    TLayoutSectionContent extends StoredLayoutSectionItemContent
+>(
+    state: LayoutBaseState<TLayoutSource, TLayoutItem, TLayoutItemContent, TLayoutSection, TLayoutSectionContent>, 
+    owner: string, name: string, version: number
+): TLayoutItem | null => {
+    const content = getLayoutItemContent(state, owner, name, version);
+    if (!content) return null;
+    
+    const sections = getLayoutSectionItemContents(state, content.id);
+    
+    return {
+        ...content,
+        sections,
+    } as unknown as TLayoutItem;
+};
+
+
+
+export const getLayoutItemContent = <
+    TLayoutSource extends LayoutBaseSource,
+    TLayoutItem extends StoredLayoutItem<TLayoutSection>,
+    TLayoutItemContent extends StoredLayoutItemContent,
+    TLayoutSection extends StoredLayoutSectionItem,
+    TLayoutSectionContent extends StoredLayoutSectionItemContent
+>(  
+    state: LayoutBaseState<TLayoutSource, TLayoutItem, TLayoutItemContent, TLayoutSection, TLayoutSectionContent>,
+    owner: string, name: string, version: number
+) => {
+    const key = Object.keys(state.byId).find(id => id.startsWith(`${owner}@${name}/${version}`));
+    return key ? state.byId[key] as TLayoutItemContent : null;
+};
+
+export const getLayoutSectionItemContents = <
+    TLayoutSource extends LayoutBaseSource,
+    TLayoutItem extends StoredLayoutItem<TLayoutSection>,
+    TLayoutItemContent extends StoredLayoutItemContent,
+    TLayoutSection extends StoredLayoutSectionItem,
+    TLayoutSectionContent extends StoredLayoutSectionItemContent
+>(
+    state: LayoutBaseState<TLayoutSource, TLayoutItem, TLayoutItemContent, TLayoutSection, TLayoutSectionContent>, 
+    layoutId: string | null
+) => {
+    if (!layoutId) return [];
+    return Object.values(state.sectionById).filter(s => s.layoutId() === layoutId) as TLayoutSectionContent[];
 };
