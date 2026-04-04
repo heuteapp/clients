0151cdee8656ffa974478da621acde2c01836096
k4yr2 <kyrzkaya@gmail.com>
2026-04-05 00:33:53 +0300

    feat(layout): update LayoutStyle and LayoutSectionStyle for improved type definitions

--- apps/web/src/modules/layout/types/layout.style.types.ts
+++ apps/web/src/modules/layout/types/layout.style.types.ts
@@ -3,9 +3,14 @@ import { LayoutBase, LayoutSectionBase } from "./layout.base.types";
 
 export interface LayoutStyle extends LayoutBase{
     box: BoxStyle;
+    sections: LayoutSectionStyle[];
 }
 
 export interface LayoutSectionStyle extends LayoutSectionBase {
     box: BoxStyle;
     grid?: BoxStyle;
 }
+
+export type LayoutStyleContent = Omit<LayoutStyle, "sections">;
+
+export type LayoutSectionStyleContent = LayoutSectionStyle;
