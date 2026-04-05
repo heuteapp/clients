f0ffa07aa4a7af6c1759cf94d45af36c69bfaca1
k4yr2 <kyrzkaya@gmail.com>
2026-04-05 18:18:05 +0300

    feat(machine): implement workspace dailyboard state machine and service

---
+++ apps/web/src/modules/workspace-dailyboard/state/auth.machine.ts
@@ -0,0 +1,23 @@
+import { createActor, createMachine, setup } from "xstate";
+import { WorkspaceDailyboardMachineContext, WorkspaceDailyboardMachineEvent } from "../types/state/workspace-dailyboard.machine.types";
+
+export const workspaceDailyboardMachine = setup({
+    types: {
+        context: {} as WorkspaceDailyboardMachineContext,
+        events: {} as WorkspaceDailyboardMachineEvent
+    }
+
+    
+}).createMachine({
+    context: {
+
+    },
+    id: "workspace-dailyboard",
+    initial: "fetching",
+    states: {
+        "fetching": {
+        }
+    }
+});
+
+export const workspaceDailyboardService = createActor(workspaceDailyboardMachine);
