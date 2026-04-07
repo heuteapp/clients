aa4afe5fa87236c9784fb896cd4c109719ce65a0
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 00:53:28 +0300

    feat(dom.utils): add getCellAtCursor function for cell position retrieval

--- apps/web/src/modules/ui-layout/utils/dom.utils.ts
+++ apps/web/src/modules/ui-layout/utils/dom.utils.ts
@@ -1,3 +1,5 @@
+import { Pointer } from "../../shared/types/common";
+
 export const findGridElement = (clientX: number, clientY: number): HTMLDivElement | null => {
     const element = document.elementFromPoint(clientX, clientY);
     return element?.closest<HTMLDivElement>("[data-layout-grid]") || null;
@@ -13,4 +15,10 @@ export const getGridMeta = (gridEl: HTMLDivElement) => {
     };
     
     return { sectionSize };
+}
+
+export const getCellAtCursor = (client: Pointer, gridRect: DOMRect, cellSize: number) => {
+    const col = Math.floor((client.x - gridRect.left) / cellSize);
+    const row = Math.floor((client.y - gridRect.top) / cellSize);
+    return { col, row };
 }
