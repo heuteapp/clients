742a9c83006ebccde9f9ddd7c2b2d6736504854b
k4yr2 <kyrzkaya@gmail.com>
2026-04-07 21:32:01 +0300

    fix(DailyboardCard): update data references for consistency and clarity

--- apps/web/src/modules/ui-dailyboard/components/DailyboardCard.tsx
+++ apps/web/src/modules/ui-dailyboard/components/DailyboardCard.tsx
@@ -5,21 +5,34 @@ import { DailyboardCardProps } from "@/src/modules/ui-dailyboard/types/dailyboar
 import { useLayoutEffect, useRef } from "react";
 
 function DailyboardCard(props : DailyboardCardProps) {
-    const { data } = props;
+    const { data: dailyboardCardData } = props;
     const { registry} = useDailyboardContext();
     const ref = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
-        registry.registerDailyboardCard(data.id, ref, props)
+        registry.registerDailyboardCard(dailyboardCardData.id, ref, props)
 
         return () => {
-            registry.unregisterDailyboardCard(data.id)
+            registry.unregisterDailyboardCard(dailyboardCardData.id)
         }
     }, [registry]) 
 
+    const dailyboardCardContent = dailyboardCardData.content;
+    const placement = dailyboardCardData.placement?.position;
+
+    const placementAttrs = placement?.colIndex !== undefined ? {
+        'data-dailyboard-card-placed': true,
+        'data-dailyboard-card-colIndex': placement.colIndex,
+        'data-dailyboard-card-rowIndex': placement.rowIndex,
+        'data-dailyboard-card-colSpan': placement.colSpan,
+        'data-dailyboard-card-rowSpan': placement.rowSpan,
+    } : {};
+
     return (
         <div 
-            data-dailyboard-card-id={data.id}
+            data-dailyboard-card-id={dailyboardCardData.id}
+            data-dailyboard-card-title={dailyboardCardContent.title}
+            {...placementAttrs}
             className={style.card}
             ref={ref}
         >
