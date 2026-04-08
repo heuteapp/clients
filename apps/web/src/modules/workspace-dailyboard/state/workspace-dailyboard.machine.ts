b03f3b5dba7a94f40721fec1520fc6cd1ddd93ca
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 15:28:24 +0300

    refactor(workspace-dailyboard): add createCard action to workspaceDailyboardMachine

--- apps/web/src/modules/workspace-dailyboard/state/workspace-dailyboard.machine.ts
+++ apps/web/src/modules/workspace-dailyboard/state/workspace-dailyboard.machine.ts
@@ -1,7 +1,7 @@
 import { createActor, setup } from "xstate";
 import { WorkspaceDailyboardMachineContext, WorkspaceDailyboardMachineEvent, WorkspaceDailyboardMachineState } from "../types/state/workspace-dailyboard.machine.types";
 import { fetchSourcesActor } from "./workspace-dailyboard.actors";
-import { resolveSourcesAction } from "./workspace-dailyboard.actions";
+import { createCardAction, resolveSourcesAction } from "./workspace-dailyboard.actions";
 
 export const workspaceDailyboardMachine = setup({
     types: {
@@ -13,6 +13,7 @@ export const workspaceDailyboardMachine = setup({
     }, 
     actions: {
         resolveSources: resolveSourcesAction,
+        createCard: createCardAction
     }
 }).createMachine({
     /** @xstate-layout N4IgpgJg5mDOIC5QHcD2AnA1rADgQwGMwBaCPASwBsBPAI1T3QgDpkKAXcgOymfIkpgAxADEAogBUAwgAkA+gGUA8gFUASlLEKA2gAYAuolA5Uscp1RcjIAB6IAjLoAsAdmYBmJ+5eeXT+wBM9vYANCDUiACs9gAczLouAGzRiZ4xAU4ZAL5ZYWhYuIQkZFR0DEysHNy8AGZg7AQAFtUABLCoAK7oRLBCEJZgfFwAbqiYg3UNjcTtXT16hkggJmYWVkt2CJEBYREIMfbMkbon9i4AnO7Rkck5eRjY+ESkFDT0jCxs5tXMk02ts26cCEYHQ6AwzBwlDw7BqGAAtr96k0Zp0gbAFtYVt9LNZNo5XB4vD5XP4gqFwohXLojklIk5EjFkuldJFIncQPlHkUXqV3hV0GA8BBqHwBMIpGoxABBCRiORSaVqAAimKW2LWeMQ7kSNMi7kCvjJjnOTl2iAC50O5zpyTOqUSficHK5hWeJTe5RYguFov4glEklkilUGi0auMphx61Am0S5ziugNASNgRN5oQnnOzHSyUSQQyuvOiRdDzdxVeZQ+zB9IuYBB9nB4LQIHyEkplcoVSuVihUUk0YmVQ4jyyjmo2FsS1pixMiGc8AWYrjz0SSOqdpYKTwrfK9NaFdYbQqbUBbbY7svlipVchE0oAkgAZEcGLHj8i4ycIALT5gJucF3OJcbWSTxmRcGJLi3bl3UrflvUPUVjxhVpWyYdspSvbtb0VAA5TQnxfVU33VD8v1jKcZ0AykECcLxlzpcDIhiSDoI5LhUAgOBrFdHdeU9D531WT8Y1sRBiHODNiBLXJOTLfiPSrCovlPYToy1fYXCXTxvFTIJdCk2irjiSIkmnM53ELexUhg8sBOUz4qh4MVBHUidKIQYCaV0kk-DTQyM1cSJmHsSIE0SJxTSTSKmTsxT4P3VSfj+Zpm0BHp3NEzTAn8I4wqLfzySChl4jpBkmRYgJWXZOS+J5JSEIPX0soo8TMyMvZqqcRjV3tDd6PihrEurWs-XFVqxM2elDiTQ1SQCzrtWODxfxuAsGUM2T7m3Ya91GpD60bNChLIkS2s2ViF10Jdtj69dHQZHIciAA */
@@ -71,7 +72,8 @@ export const workspaceDailyboardMachine = setup({
                 "creating card": {
                     on: {
                         CREATE_CARD_SUCCEEDED: {
-                            target: "idle"
+                            target: "idle",
+                            actions: ["createCard"]
                         },
                         CREATE_CARD_FAILED: {
                             target: "idle"
