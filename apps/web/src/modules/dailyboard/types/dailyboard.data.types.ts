91f568946dffb50a1dfd02091f40e16d2ef2ae69
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 12:51:16 +0300

    feat(dailyboard): add DailyboardData and DailyboardCardData interfaces

---
+++ apps/web/src/modules/dailyboard/types/dailyboard.data.types.ts
@@ -0,0 +1,22 @@
+import { GridRect } from "../../shared/types/common";
+import { DailyboardBase, DailyboardCardBase } from "./dailyboard.base.types";
+
+export interface DailyboardData extends DailyboardBase {
+    cards: DailyboardCardData[];
+}
+
+export interface DailyboardCardData extends DailyboardCardBase {
+    material: DailyboardCardMaterial;
+    placement: DailyboardCardPlacement | null;
+}
+
+//
+
+export interface DailyboardCardMaterial {
+    title: string | null;
+}
+
+export interface DailyboardCardPlacement {
+    sectionName: string;
+    position: GridRect;
+}
