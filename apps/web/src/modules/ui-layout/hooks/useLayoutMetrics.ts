2cc6374a0fd5b20e105735103c996c69762e8c11
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 22:01:23 +0300

    feat(metrics): refactor useLayoutMetrics to utilize subscription for layout updates

--- apps/web/src/modules/ui-layout/hooks/useLayoutMetrics.ts
+++ apps/web/src/modules/ui-layout/hooks/useLayoutMetrics.ts
@@ -3,22 +3,20 @@ import { LayoutMetrics } from "../types/layout.metrics";
 import { applyLayoutMetrics, calculateLayoutMetrics } from "../metrics/layout.metrics";
 import { LayoutRegistry } from "../types/layout.registry";
 import { StoredLayoutData, StoredLayoutStyle } from "@/src/heute-store/types/layout.types";
+import { useMetricsContext } from "../../ui-shared/hooks/useMetricsContext";
 
 export const useLayoutMetrics = (registry: LayoutRegistry, dataSource: StoredLayoutData | null, styleSource: StoredLayoutStyle | null) : LayoutMetrics => {
+    const { subscribe, unsubscribe } = useMetricsContext();
     const metrics = React.useRef<LayoutMetrics>(null);
 
     React.useEffect(() => {
-        const resizeObserver = new ResizeObserver(() => {
+        subscribe("layout", () => {
             metrics.current = calculateLayoutMetrics({ registry, dataSource, styleSource });
             applyLayoutMetrics({ registry, metrics: metrics.current!, styleSource });
         });
 
-        if (registry.layout.ref.current) {
-            resizeObserver.observe(registry.layout.ref.current);
-        }
-
         return () => {
-            resizeObserver.disconnect();
+            unsubscribe("layout");
         }
     }, [dataSource, styleSource]);
 
