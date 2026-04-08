84071ec03591d4184e06916e673a9e2d0974bbde
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 20:25:43 +0300

    feat(dailyboard.metrics): add useDailyboardMetrics hook for managing Dailyboard metrics

---
+++ apps/web/src/modules/ui-dailyboard/hooks/useDailyboardMetrics.ts
@@ -0,0 +1,26 @@
+import React from "react";
+import { DailyboardMetrics } from "../types/dailyboard.metrics";
+import { applyDailyboardMetrics, calculateDailyboardMetrics } from "../metrics/dailyboard.metrics";
+import { DailyboardRegistry } from "../types/dailyboard.registry";
+import { LayoutMetrics } from "../../ui-layout/types/layout.metrics";
+
+export const useDailyboardMetrics = (registry: DailyboardRegistry, layoutMetrics: LayoutMetrics) : DailyboardMetrics => {
+    const metrics = React.useRef<DailyboardMetrics>(null);
+
+    React.useEffect(() => {
+        const resizeObserver = new ResizeObserver(() => {
+            metrics.current = calculateDailyboardMetrics(layoutMetrics);
+            applyDailyboardMetrics(registry, metrics.current!);
+        });
+
+        if (registry.dailyboard.ref.current) {
+            resizeObserver.observe(registry.dailyboard.ref.current);
+        }
+
+        return () => {
+            resizeObserver.disconnect();
+        }
+    }, [registry, layoutMetrics]);
+
+    return metrics.current!;
+}
