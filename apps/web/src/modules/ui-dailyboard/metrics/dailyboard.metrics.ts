e814035a68e2be66eda11f93d94270fe39fd4deb
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 20:23:32 +0300

    feat(dailyboard.metrics): add applyDailyboardMetrics function to handle DailyboardMetrics

--- apps/web/src/modules/ui-dailyboard/metrics/dailyboard.metrics.ts
+++ apps/web/src/modules/ui-dailyboard/metrics/dailyboard.metrics.ts
@@ -1,8 +1,13 @@
 import { DailyboardMetrics } from "../types/dailyboard.metrics";
 import { LayoutMetrics } from "../../ui-layout/types/layout.metrics";
+import { DailyboardRegistry } from "../types/dailyboard.registry";
 
 export const calculateDailyboardMetrics = (layoutMetrics: LayoutMetrics) : DailyboardMetrics => {
     return {
         layout: layoutMetrics
     }
+}
+
+export const applyDailyboardMetrics = (registry: DailyboardRegistry, metrics: DailyboardMetrics) => {
+    
 }
