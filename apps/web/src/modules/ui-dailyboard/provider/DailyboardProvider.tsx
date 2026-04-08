98759b821fae4a9c158ed1e3242b1a597c5eba46
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 20:26:08 +0300

    feat(dailyboard.context): add metrics to DailyboardContextValue and update DailyboardProvider to use dailyboard metrics

--- apps/web/src/modules/ui-dailyboard/provider/DailyboardProvider.tsx
+++ apps/web/src/modules/ui-dailyboard/provider/DailyboardProvider.tsx
@@ -5,16 +5,19 @@ import { useLayoutContext } from "@/src/modules/ui-layout/hooks/useLayoutContext
 import { useDailyboardRegistry } from "@/src/modules/ui-dailyboard/hooks/useDailyboardRegistry";
 import { DailyboardContext } from "@/src/modules/ui-dailyboard/contexts/dailyboard.context";
 import { DailyboardProviderProps } from "../types/dailyboard.props";
+import { useDailyboardMetrics } from "../hooks/useDailyboardMetrics";
 
 export function DailyboardProvider({ dataSource, children }: DailyboardProviderProps) {
     const layout = useLayoutContext();
 
     const dailyboardRef = React.useRef<HTMLDivElement | null>(null);
     const registry = useDailyboardRegistry(dailyboardRef, layout.registry);
+
+    const metrics = useDailyboardMetrics(registry, layout.metrics);
     
     const contextValue = React.useMemo(() => ({
-        dataSource, layout, registry
-    }), [dataSource, layout, registry]);
+        dataSource, layout, registry, metrics
+    }), [dataSource, layout, registry, metrics]);
 
     return (
         <DailyboardContext.Provider value={contextValue}>
--- apps/web/src/modules/ui-dailyboard/types/dailyboard.context.ts
+++ apps/web/src/modules/ui-dailyboard/types/dailyboard.context.ts
@@ -1,9 +1,11 @@
 import { LayoutContextValue } from "@/src/modules/ui-layout/types/layout.context";
 import { DailyboardRegistry } from "./dailyboard.registry";
 import { StoredDailyboardData} from "@/src/heute-store/types/dailyboard.types";
+import { DailyboardMetrics } from "./dailyboard.metrics";
 
 export interface DailyboardContextValue {    
     dataSource: StoredDailyboardData | null;
     layout: LayoutContextValue;
     registry: DailyboardRegistry;    
+    metrics: DailyboardMetrics;
 }
