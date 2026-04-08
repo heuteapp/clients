66a34bbcfb6445ae4de4f09ee802a73e5c19d9e5
k4yr2 <kyrzkaya@gmail.com>
2026-04-08 20:21:35 +0300

    refactor(dailyboard.metrics): rename calcDailyboardMetrics to calculateDailyboardMetrics and simplify parameters

--- apps/web/src/modules/ui-dailyboard/metrics/dailyboard.metrics.ts
+++ apps/web/src/modules/ui-dailyboard/metrics/dailyboard.metrics.ts
@@ -1,12 +1,7 @@
-import { StoredLayoutData, StoredLayoutStyle } from "@/src/heute-store/types/layout.types";
-import { useLayoutMetrics } from "../../ui-layout/hooks/useLayoutMetrics";
-import { LayoutRegistry } from "../../ui-layout/types/layout.registry";
 import { DailyboardMetrics } from "../types/dailyboard.metrics";
+import { LayoutMetrics } from "../../ui-layout/types/layout.metrics";
 
-export const calcDailyboardMetrics = (registry: LayoutRegistry, dataSource: StoredLayoutData | null, styleSource: StoredLayoutStyle | null) : DailyboardMetrics => {
-    
-    const layoutMetrics = useLayoutMetrics(registry, dataSource, styleSource);
-
+export const calculateDailyboardMetrics = (layoutMetrics: LayoutMetrics) : DailyboardMetrics => {
     return {
         layout: layoutMetrics
     }
