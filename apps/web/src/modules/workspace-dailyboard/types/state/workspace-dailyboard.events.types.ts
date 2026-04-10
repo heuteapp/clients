853e7a790e5ac700488f6df00adc444c3057a1f6
k4yr2 <kyrzkaya@gmail.com>
2026-04-10 16:47:31 +0300

    refactor(events): simplify event type definitions for FetchSources and CreateCard

--- apps/web/src/modules/workspace-dailyboard/types/state/workspace-dailyboard.events.types.ts
+++ apps/web/src/modules/workspace-dailyboard/types/state/workspace-dailyboard.events.types.ts
@@ -4,18 +4,17 @@ import { GridSize } from "@/src/modules/shared/types/common";
 import { YYMMDDDate } from "@/src/modules/shared/types/date.types";
 import { DoneActorEvent, ErrorActorEvent } from "xstate";
 
-export type FetchSourcesEvents = FETCH_SOURCES_REQUESTED_EVENT | DONE_FETCH_SOURCES_EVENT | ERROR_FETCH_SOURCES_EVENT;
+export type FetchSourcesEvent =
+  | { type: "FETCH_SOURCES_REQUESTED"; dailyboardPath: string }
+  | DoneActorEvent<DailyboardResponse, "fetch-sources">
+  | ErrorActorEvent<"fetch-sources">;
 
-export type FETCH_SOURCES_REQUESTED_EVENT = { type: "FETCH_SOURCES_REQUESTED", dailyboardPath: string };
-
-export type DONE_FETCH_SOURCES_EVENT = DoneActorEvent<DailyboardResponse, "fetch-sources">;
-
-export type ERROR_FETCH_SOURCES_EVENT = ErrorActorEvent<"fetch-sources">;
-
-export type CreateCardEvents = CREATE_CARD_REQUESTED_EVENT | CREATE_CARD_SUCCEEDED_EVENT | CREATE_CARD_CANCELLED_EVENT;
-
-export type CREATE_CARD_REQUESTED_EVENT = { type: "CREATE_CARD_REQUESTED", cardSize: GridSize };
-
-export type CREATE_CARD_SUCCEEDED_EVENT = { type: "CREATE_CARD_SUCCEEDED", categoryPath: string, date: YYMMDDDate, placement: DailyboardCardPlacement };
-
-export type CREATE_CARD_CANCELLED_EVENT = { type: "CREATE_CARD_CANCELLED" };
+export type CreateCardEvent =
+  | { type: "CARD_CREATE_REQUESTED"; cardSize: GridSize }
+  | {
+      type: "CARD_CREATE_SUCCEEDED";
+      categoryPath: string;
+      date: YYMMDDDate;
+      placement: DailyboardCardPlacement;
+    }
+  | { type: "CARD_CREATE_CANCELLED" };
