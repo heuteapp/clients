14098867296bdcb4bc1458a4ecf77ff4432ee0c3
k4yr2 <kyrzkaya@gmail.com>
2026-04-10 19:57:17 +0300

    fix(events): add EditCardEvent to WorkspaceDailyboardMachineEvent type

--- apps/web/src/modules/workspace-dailyboard/types/state/workspace-dailyboard.machine.types.ts
+++ apps/web/src/modules/workspace-dailyboard/types/state/workspace-dailyboard.machine.types.ts
@@ -1,5 +1,5 @@
 import { ActorRefFrom } from "xstate";
-import { FetchSourcesEvent, CreateCardEvent } from "./workspace-dailyboard.events.types";
+import { FetchSourcesEvent, CreateCardEvent, EditCardEvent } from "./workspace-dailyboard.events.types";
 import { workspaceDailyboardMachine } from "../../state/workspace-dailyboard.machine";
 import { StoredDailyboardData } from "@/src/heute-store/types/dailyboard.types";
 import { StoredLayoutData, StoredLayoutStyle } from "@/src/heute-store/types/layout.types";
@@ -22,6 +22,7 @@ export type WorkspaceDailyboardMachineContext = {
 export type WorkspaceDailyboardMachineEvent = 
   | FetchSourcesEvent
   | CreateCardEvent
+  | EditCardEvent
 
 //
 
