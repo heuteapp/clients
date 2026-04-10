ca4c559d2bbedba8e1cf6f12cd1adcd480704948
k4yr2 <kyrzkaya@gmail.com>
2026-04-10 23:33:20 +0300

    fix(events): refactor editing state management for clarity and efficiency

--- apps/web/src/modules/workspace-dailyboard/hooks/state/useEditCardState.ts
+++ apps/web/src/modules/workspace-dailyboard/hooks/state/useEditCardState.ts
@@ -13,61 +13,77 @@ export const useEditCardState = () => {
 
     useEffect(() => {
         console.log(state.value);
+        checkEditingState();
+    }, [state]);
+
+    const checkEditingState = () => {
+        const entryEditingState = () => {
+            removeEditRequestListener();
+            addOutsideClickListener();
+            initListener.current = false;
+        }
+
+        const exitEditingState = () => {
+            addEditRequestListener();
+            removeOutsideClickListener();
+            initListener.current = true;
+            currentCard.current = null;
+        }
+
+        const handleEditRequest = (event: MouseEvent) => {
+            const { clientX, clientY } = event;
+            
+            const card = findDailyboardCardAtCursor(clientX, clientY);
+
+            if(card) {
+                currentCard.current = card;
+                
+                const data = getDailyboardCardData(card);
+                send({ type: "CARD_EDIT_REQUESTED", cardKey: data.key })
+            }
+        }
+
+        const handleOutsideClick = (event: PointerEvent) => {
+            if (!currentCard.current) return;
+            
+            const { clientX, clientY } = event;
+            
+            const clickedCard = findDailyboardCardAtCursor(clientX, clientY);
+            
+            if (clickedCard !== currentCard.current) {
+                currentCard.current = null;
+                send({ type: "CARD_EDIT_CANCELLED" });
+            }
+        }
+
+        const addEditRequestListener = () => {
+            document.addEventListener("dblclick", handleEditRequest);
+        };
+
+        const addOutsideClickListener = () => {
+            document.addEventListener("pointerdown", handleOutsideClick);
+        };
+
+        const removeEditRequestListener = () => {
+            document.removeEventListener("dblclick", handleEditRequest);
+        };
+
+        const removeOutsideClickListener = () => {
+            document.removeEventListener("pointerdown", handleOutsideClick);
+        };
+        
+        //
+
+
         if (isEditingCard(state)) {
             if(initListener.current) {
-                removeEditRequestListener();
-                addOutsideClickListener();
-                initListener.current = false;
+                entryEditingState();
             }
         }
         else {
             if(!initListener.current) {
-                addEditRequestListener();
-                removeOutsideClickListener();
-                initListener.current = true;
+                exitEditingState();
             }
         }
-    }, [state]);
-
-    const handleEditRequest = (event: MouseEvent) => {
-        const { clientX, clientY } = event;
-        
-        const card = findDailyboardCardAtCursor(clientX, clientY);
-
-        if(card) {
-            currentCard.current = card;
-            
-            const data = getDailyboardCardData(card);
-            send({ type: "CARD_EDIT_REQUESTED", cardKey: data.key })
-        }
     }
-
-    const handleOutsideClick = (event: PointerEvent) => {
-        if (!currentCard.current) return;
-        
-        const { clientX, clientY } = event;
-        
-        const clickedCard = findDailyboardCardAtCursor(clientX, clientY);
-        
-        if (clickedCard !== currentCard.current) {
-            currentCard.current = null;
-            send({ type: "CARD_EDIT_CANCELLED" });
-        }
-    }
-
-    const addEditRequestListener = () => {
-        document.addEventListener("dblclick", handleEditRequest);
-    };
-
-    const addOutsideClickListener = () => {
-        document.addEventListener("pointerdown", handleOutsideClick);
-    };
-
-    const removeEditRequestListener = () => {
-        document.removeEventListener("dblclick", handleEditRequest);
-    };
-
-    const removeOutsideClickListener = () => {
-        document.removeEventListener("pointerdown", handleOutsideClick);
-    };
 }
