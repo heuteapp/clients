1b66b7fcf652a8504b534d133373941d9498025f
k4yr2 <kyrzkaya@gmail.com>
2026-04-11 16:32:26 +0300

    feat(ghostCard): enhance useGhostCard hook with card size management and lifecycle handling

--- apps/web/src/modules/workspace-dailyboard/hooks/helpers/useGhostCard.ts
+++ apps/web/src/modules/workspace-dailyboard/hooks/helpers/useGhostCard.ts
@@ -1,11 +1,60 @@
-import { useState } from "react";
+import { GridRect, GridSize, Rect } from "@/src/modules/shared/types/common";
+import { findDailyboardInSubtree } from "@/src/modules/ui-dailyboard/utils/dom.utils";
+import { useEffect, useRef, useState } from "react";
 
 export const useGhostCard = () => {
     const [isActive, setIsActive] = useState(false);
+    const [cardSize, setCardSize] = useState<GridSize | null>(null);
 
-    const start = () => {
+    const dailyboardElement = useRef<HTMLDivElement | null>(null);
+    const sectionElement = useRef<HTMLDivElement | null>(null);
+    const gridElement = useRef<HTMLDivElement | null>(null);
+
+    const ghostCardElement = useRef<HTMLDivElement | null>(null);
+    const ghostCardGridPos = useRef<GridRect | null>(null);
+    const ghostCardPos = useRef<Rect | null>(null);
+    const isGhostCardOverlapping = useRef<boolean>(false);
+
+    const suggestedCardElement = useRef<HTMLDivElement | null>(null);
+    const suggestedCardGridPos = useRef<GridRect | null>(null);
+    const suggestedCardPos = useRef<Rect | null>(null);
+
+    useEffect(() => {
+        if(isActive && cardSize) {
+            dailyboardElement.current = findDailyboardInSubtree(document.body);
+
+            createGhostCard();
+        }
+        else {
+            dailyboardElement.current = null;
+
+            removeGhostCard();
+        }
+    }, [isActive]);
+
+    const createGhostCard = () => {
+        if(!dailyboardElement.current) return;
+        if(ghostCardElement.current) return;
+
+        ghostCardElement.current = document.createElement("div");
+        ghostCardElement.current.id = "dailyboard-ghost-card";
+
+        document.body.appendChild(ghostCardElement.current);
+    }
+
+    const removeGhostCard = () => {
+        if(!ghostCardElement.current) return;
+
+        document.body.removeChild(ghostCardElement.current);
+        ghostCardElement.current = null;
+    }
+
+    //
+
+    const start = (cardSize: GridSize) => {
         if(!isActive) {
             setIsActive(true);
+            setCardSize(cardSize);
             return true;
         }
 
@@ -15,6 +64,7 @@ export const useGhostCard = () => {
     const finish = () => {
         if(isActive) {
             setIsActive(false);
+            setCardSize(null);
             return true;
         }
 
