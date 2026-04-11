7fc2b1552b726e0c3fa406684d067cbfa3a01237
k4yr2 <kyrzkaya@gmail.com>
2026-04-11 16:42:37 +0300

    feat(ghostCard): enhance useGhostCard hook with advanced position calculation and overlapping detection

--- apps/web/src/modules/workspace-dailyboard/hooks/helpers/useGhostCard.ts
+++ apps/web/src/modules/workspace-dailyboard/hooks/helpers/useGhostCard.ts
@@ -1,10 +1,15 @@
 import { GridRect, GridSize, Rect } from "@/src/modules/shared/types/common";
-import { findDailyboardInSubtree } from "@/src/modules/ui-dailyboard/utils/dom.utils";
+import { isGridRectOverlappingSome, findBestGridRectPosition } from "@/src/modules/shared/utils/common";
+import { calcDailyboardCardFixedRect, calcDailyboardCardGridIndexes, findDailyboardCardsForSection, findDailyboardClosest, findDailyboardInSubtree, getDailyboardCardData } from "@/src/modules/ui-dailyboard/utils/dom.utils";
+import { useLayoutContext } from "@/src/modules/ui-layout/hooks/useLayoutContext";
+import { findGridAtPoint, findSectionClosest, getSectionDataForGrid, calcGridPointerAtCursor } from "@/src/modules/ui-layout/utils/dom.utils";
 import { useEffect, useRef, useState } from "react";
 
 export const useGhostCard = () => {
     const [isActive, setIsActive] = useState(false);
     const [cardSize, setCardSize] = useState<GridSize | null>(null);
+    
+    const { metrics } = useLayoutContext();
 
     const dailyboardElement = useRef<HTMLDivElement | null>(null);
     const sectionElement = useRef<HTMLDivElement | null>(null);
@@ -31,6 +36,85 @@ export const useGhostCard = () => {
             removeGhostCard();
         }
     }, [isActive]);
+
+    //
+
+    const calculatePosition = (clientX: number, clientY: number) => {
+        if(!isActive || !cardSize) return;
+
+        const size = {
+            width: ((metrics.cellSize.grid || 0) * cardSize.colSpan),
+            height: ((metrics.cellSize.grid || 0) * cardSize.rowSpan)
+        }
+
+        const ghostCardEl = ghostCardElement.current!;
+
+        const gridEl = gridElement.current = findGridAtPoint(clientX, clientY);
+        const sectionEl = sectionElement.current = gridElement.current ? findSectionClosest(gridElement.current) : null;
+        const dailyboardEl = dailyboardElement.current = sectionElement.current ? findDailyboardClosest(sectionElement.current) : null;
+
+        const isOverlapping = isGhostCardOverlapping.current = false;
+
+        if(
+            gridEl && 
+            sectionEl && 
+            dailyboardEl
+        ) {
+            const { name: sectionName, position: gridPos } = getSectionDataForGrid(gridEl)!;
+
+            const gridRect = gridEl.getBoundingClientRect();
+            const cellSize = metrics.cellSize.grid || 0;
+
+            const gridSize = { colSpan: gridPos.colSpan, rowSpan: gridPos.rowSpan };
+            const { col: mouseCol, row: mouseRow } = calcGridPointerAtCursor({ x: clientX, y: clientY }, gridRect, cellSize);
+            let { col: cardCol, row: cardRow } = calcDailyboardCardGridIndexes(mouseCol, mouseRow, gridSize, cardSize);
+
+            ghostCardGridPos.current = { colIndex: cardCol, rowIndex: cardRow, colSpan: cardSize.colSpan, rowSpan: cardSize.rowSpan };
+
+            const gap = 8;
+            ghostCardPos.current = calcDailyboardCardFixedRect(gridRect, gap, gridSize, ghostCardGridPos.current);
+
+            const cards = findDailyboardCardsForSection(dailyboardEl, sectionName);
+
+            const cardRects = cards.map(getDailyboardCardData);
+            isGhostCardOverlapping.current = isGridRectOverlappingSome(ghostCardGridPos.current, cardRects);
+
+            if(isOverlapping) {
+                const bestPos = findBestGridRectPosition(ghostCardGridPos.current, cardRects, gridSize);
+
+                if (bestPos) {
+                    suggestedCardGridPos.current = bestPos;
+                    suggestedCardPos.current = calcDailyboardCardFixedRect(gridRect, gap, gridSize, suggestedCardGridPos.current);
+                }
+                else {
+                    suggestedCardGridPos.current = null;
+                    suggestedCardPos.current = null;
+                }
+            }
+            else {
+                ghostCardEl.classList.remove("overlapping");
+                suggestedCardGridPos.current = null;
+                suggestedCardPos.current = null;
+            }
+        }
+        else {
+            sectionElement.current = null;
+
+            ghostCardGridPos.current = null;
+
+            ghostCardPos.current = {
+                x: clientX - (size.width / 2),
+                y: clientY - (size.height / 2),
+                width: size.width,
+                height: size.height
+            };
+
+            suggestedCardGridPos.current = null;
+            suggestedCardPos.current = null;
+        }
+    };
+
+    //
 
     const createGhostCard = () => {
         if(!dailyboardElement.current) return;
