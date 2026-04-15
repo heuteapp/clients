406d6f707fe0746db59803b8941eb1ec3dc19b58
k4yr2 <kyrzkaya@gmail.com>
2026-04-15 11:34:44 +0300

    feat: add SignInEvent type to auth event types for improved authentication handling

--- apps/web/src/modules/auth/types/auth.events.types.ts
+++ apps/web/src/modules/auth/types/auth.events.types.ts
@@ -1,8 +1,10 @@
+import { SignInRequest } from "@/src/api/models/requests/auth.request";
 import { AuthSession } from "./auth.types";
 
 export type AuthEvent =
     | SessionHydrateEvent
-    | SessionRefreshEvent;
+    | SessionRefreshEvent
+    | SignInEvent;
 
 export type SessionHydrateEvent = 
     | { type: 'SESSION_HYDRATE_REQUEST'; }
@@ -13,3 +15,8 @@ export type SessionRefreshEvent =
     | { type: 'SESSION_REFRESH_REQUEST'; input: AuthSession; }
     | { type: 'SESSION_REFRESH_SUCCESS'; output: AuthSession; }
     | { type: 'SESSION_REFRESH_FAILURE'; error: string; }
+
+export type SignInEvent =
+    | { type: 'SIGN_IN_REQUEST'; input: SignInRequest; }
+    | { type: 'SIGN_IN_SUCCESS'; output: AuthSession; }
+    | { type: 'SIGN_IN_FAILURE'; error: string; }
