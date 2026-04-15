530f2addf2b6354e2f2135d03c0964c08e8cd981
k4yr2 <kyrzkaya@gmail.com>
2026-04-15 11:17:50 +0300

    feat: refactor auth machine states for improved session management flow

--- apps/web/src/modules/auth/state/auth.machine.ts
+++ apps/web/src/modules/auth/state/auth.machine.ts
@@ -19,39 +19,51 @@ export const authMachine = setup({
     isRegistrationAwaiting: ({ context }) => !!context.registration
   },
 }).createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QEMCuAXAFgOgMabFwGsBLAOygAI0sBiCAezLG3IDcGiX9CiBaGpgDaABgC6iUAAcGsEuhJNJIAB6IAzOoBM2EeoCMWgGz7NAVi0itAFgA0IAJ6J9+6-uwB2fSICc6kfpGvh7W1gC+YfaCeATE5FSCtGAATskMydhSADbI6ABm6QC2MbwCGMLiyjJyCkpIqhraugbGpuoWVnaOiBY+un4hPoFGWmZmHhFR5SVxFJTJYFAksOjJuYpk9Ews7JzcsfwLSytrtWSiEvXV8hvKaggAHD4PnmYielra6n76PvZOCA62BMHlGPhE1iePjMkxA0R4syoR2Wq3WTCSqXSmRy+SKM0OixRpw2FyqshudVA9yeLw8bw+Xx+f26j3c9ICPmeWh8NgeE0icOmgjAZAUuFykFoAGUAJIAcQAcgB9ADyAFUACqkq7ks53RBaGwiYGglyuAIPB5af7OQxGbDPazqB5GMyQswPaxGWHRVBkYWikji9CS2WKpUyhXa6S6271e6Gsx9fR8q2GiFBDw2hD6JO0+lu36fawedQ+6Z+gNiiUQaXy5VqgAK0ZA1z18YNo2s2HUdMhlrM+i8uezLp0dI8D00fLpWkM5aw2DkUDI8Uo5Dr4cjSqlaoAwnuAKJSqUtttxqnOB653RenneKwQkQPbOGKfAvkWfzgnm+Bc4ZdVzmDcw2VbcADEAEEZQAGTVAAlQ8z1jSkGhza8zFvIx73eSxrGfV8nR0N1sLMb4zFdEtwgFaJALXVApE3BtGx3fcjxPZCagvNCaVed4vk+JlsxMdRsHGKxPUHIYTAef8lxIFd6MY0ClSbJUoNghCkMqHUuNQ6lnj4hlBIMZkAWeUS3knLRLR8IxQUhOTkAAd2QG45jYFISDyIM0U2AA1Q94JlcCAE0lUPABZaCYM4ikyH1HNNA8XRJyfF0jGsD0zMQaxoWBFwfFBCFezGGEaKFVz3KoTzkm83yzloQLgrCiLotgpU9xVSLGxgw8NUPAAROL20vJLe1Sh50qMTLsuzJNjRCOcRhm-C3XKqZF1q7yHDXMBCjcrImqCkLwqimLWIPY9Tx0mM9ISjt0JvDMcMffCXxZb57Q8Sj1s9EQTC0OTtryXa5n2w7jpas72pg9SYqGkbuPuUwPGTUZ3js59PizT7OWBD0HgokJ+JMYGvNBvaDpII7mtOtqLsPAANRsZUQ4bbtbFCHrGlNnrvOdcPS7NQhS4wxkyoYSyMdRQnJurQcoWBUFwXA4FgSUkf0g0AdEkRrOeTLSznLoAUsRb6X8T5XE5Cx5Z2ygwBUKQSAWWBaBUFYJWwZA8hDZIAAp2QASloaIQYcR3ndduAtZ5tDDXwk05xcfCUytEWiodLKtA8THwQtcqBTIBgIDgZRBDJe7Er4Ixsz4TDOSb5vm-Ub0KsXBFSDmSvdPixLrGtFkXFGD8QlLESeTsuSu7XZETj8qv+8ewd3Fl3t3rzomXDMeaITEgZDX8LevCc8oRWrEMICX0aE4BlLtFCUw3lzOlX0CUSnTpZ8rH8AJ282jgSs59AzBkgDfZGBo07YHFj4PKQQsoplfO8b668hi9GdH+DuAEFJASoOQCB2scwy3cNCAwFhn5TUnK+MqwJRhtCeL4L0QNsHyUUnMBihD473DzujN4mh8IrVltmLGwJfDfG8JoFw9knJVQUB5CmDVuLniISPY09kUwAy5O0YSFFdDvD0JoGwrpJz20puDamWQuGJUCADbAg8UyhCKu6D6AIsrJjIb2NGedsIyNYRHJWKs1awA1tfPut8Ey62wN4aEIRMrfCtOoEW+9BxjGME6cYZFZL+IppHJ2Ls3bWMepYUsMDZbG1fplIebi7LZzIiIMiHgt58mohEIAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QEMCuAXAFgOmQd2QEt1CA7KbWOWQge1O0wE8IAnZE8gYgGUBRHjwCSAeQByAfQASATQAiAJQCCAFT4SeAVQDC2gTwDaABgC6iUAAdaNEvXMgAHogAcAViPZnAdncBmI85GAJwATEa+vgA0IEyIvgAsAIzYIe4BQa4hvkHxRiEAvvnRaFi4BMRkFFSwNPSMLOycULz6opKyiqrqAGJKQgAymgp8xmZIIFY2dKT2TgiJedhGyyurK4nRsQgh8c7YaUYAbF7+h4eJF76FxRg4+ERNlNTT9WwclS2CbRLD3cM8Uh+fAAipoBCpRvZJhU7OM5r4vMlcodUidXIcjK5fK5NogQmF9itsq5XEE8s5nFdriBSLQIHB7CVMFDrDCZnDEABaJLYIJ8-kC-nOQ64hDc5KCyX86lM7CsSCEeUAYyaLKmsNAc3iIVF8UOQUJyxRQWcIVNiSpN1K9wq5DVbNmiEOSzWrqMiXiutc2HiAqy6RCfNc8RltzKD0qTxq03ttnZmsQwZdbtWHtFFt8yfdpIiQS8OVD1vKj2qtQYzDeqvG0LjjvmWR9oUSpoSZqMXi8ov88WTCz1RiSOWchbuxcjpZe8oAZvLYJhKrGYxyEIdMtgEgjXF5zs5m4kRTE8SFkqtiaTycKR7hbmBSCQlRxIIuNY5E53DwgvIHe8s27lfYUhRAA */
   context: {
     auth: null,    
     registration: null,
     error: null,
   },
   id: "auth",
-  initial: "hydrating session",
+  initial: "redirecting",
   states: {
-    "awaiting session": {
-      initial: "hydrating",
+    "redirecting": {
+
+    },
+    "awaiting": {
       states: {
-        "hydrating": {
-          invoke: {
-            src: "hydrateSession",
-          },
-          on: {
-            SESSION_HYDRATE_SUCCESS: {
-              target: "#auth.authenticated",
+        "session": {
+          initial: "hydrating",
+          states: {
+            "hydrating": {
+              invoke: {
+                src: "hydrateSession",
+              },
+              on: {
+                SESSION_HYDRATE_SUCCESS: {
+                  target: "#auth.authenticated",
+                },
+                SESSION_HYDRATE_FAILURE: {
+                  target: "#auth.unauthenticated",
+                },
+                SESSION_REFRESH_REQUEST: {
+                  target: "refreshing",
+                }
+              }
             },
-            SESSION_HYDRATE_FAILURE: {
-            },
-            SESSION_REFRESH_REQUEST: {
-              target: "refreshing",
+            "refreshing": {
+
             }
           }
         },
-        "refreshing": {
-
+        "registration": {
         }
       }
     },
     "authenticated": {
+    },
+    "unauthenticated": {
     }
   },
 });
