94d33ee5c27bc3e11605cb2efccc7abacd83e324
k4yr2 <kyrzkaya@gmail.com>
2026-04-15 11:48:12 +0300

    feat: add SignInActorInput type for improved authentication input handling

--- apps/web/src/modules/auth/types/auth.actors.ts
+++ apps/web/src/modules/auth/types/auth.actors.ts
@@ -1,3 +1,4 @@
+import { SignInRequest } from "@/src/api/models/requests/auth.request";
 import { SessionHydrateEvent, SessionRefreshEvent, SignInFailureEvent, SignInSuccessEvent } from "./auth.events.types";
 
 
@@ -6,6 +7,10 @@ export type SessionHydrateActorInput = void;
 export type SessionHydrateActorEvent =
   | SessionHydrateEvent
   | SessionRefreshEvent;
+
+//
+
+export type SignInActorInput = SignInRequest;
 
 export type SignInActorEvent = 
   | SignInSuccessEvent
