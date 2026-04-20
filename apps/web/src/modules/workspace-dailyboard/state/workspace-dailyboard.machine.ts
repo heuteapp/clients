47e7328a71940eb506f79f59299c57c00b537535
k4yr2 <kyrzkaya@gmail.com>
2026-04-20 23:19:47 +0300

    refactor: update state structure in workspace dailyboard machine to streamline fetching and card creation processes

--- apps/web/src/modules/workspace-dailyboard/state/workspace-dailyboard.machine.ts
+++ apps/web/src/modules/workspace-dailyboard/state/workspace-dailyboard.machine.ts
@@ -15,7 +15,7 @@ export const workspaceDailyboardMachine = setup({
         fetchingSourcesDone: fetchingSourcesDoneAction,
     }
 }).createMachine({
-    /** @xstate-layout N4IgpgJg5mDOIC5QHcD2AnA1rADgQwGMwBaCPASwBsBPAI1T3QgDpkKAXcgOymfIkpgAxAGUA8gFUASgGEAoiID6AMTkAVGQAlFUuQEUJCtQG0ADAF1EoHKljlOqLlZAAPRADZ3AVmZeA7AAcAIxeAEwhXqZeXgEANCDUiKFeAMzMAJypQabuAekALO6B+QC+JfFoWLiEJGRUdAxMrBzcvABmYOwEABatfALC4tLySqoa2roGRmaWSCA2dg5Oc24Ifsm+gSHh0VEx8Ylr+UEZWUEFpqbb6WUVGNj4RKQUNPSMLGz2fR1dvTzMsFQAFd0ERYEIII4wHwuAA3VCYaE-HrEQEgsEzZwLL6OZyrMIHRDBZj5dJksn5fx+FLpZK3ECVB41Z71N5NT6cf7Iv68NGguBCMDodAYZg4Sh4dhtDAAW2Y3NRwP5sExc2xSzxRMuGQC+RSqRS7nC1PyhIQ7gKJNSphSlNyoRSV3pjOqTzqr0aLHQYDwEGo-UEokksgUKnUWh0+kMIhMFixthxy1Aq1Ceoy5LylICKWCITNKWpzFCNuyARtRWp7lK9K4qAgcGcLsetReDXe8cW5FxK0QxHSZuIPnJw5H6SrXmd91dLdZnuaXx4HcTmoQpJ8-mC+WOfkycQSiCCnitKWyfj1FppoUnVWbLI973nnN4-EES41PYQXjTG6CW6CO-2fcEGSfJj2yXICiKXVryZN1WzZD4Wi5ToelaN8uyTVxEF2TZN23XczXcHJThPUx0n-AIAgtGDpzvNt2SQ9oUJ5AMwHQ7tk2w9IAlw398MAw4LXSZhy0NK4-HWMir3KBkp1vd16MQhcmN+Po+TBdjMNWbIomYEIjRSUIy0CPxPDNakTkuR0LVMPwrlCQIaPk+C529X1DmsBN304hAaTNIz3CLEtTACfx0htSknOZBSEOYNy-VYzSVwCPx80LOzSLLQ0JILMoyiAA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QHcD2AnA1rADgQwGMwBaCPASwBsBPAI1T3QgDpkKAXcgOymYDMw7AgAtuvchEpgAxAGUA8gFUASgGEAorID6AMXUAVVQAkty9QEVFm-QG0ADAF1EoHKljlOqLs5AAPRACMAEwAnABszAAcACwAzJF2AQFxofEANCDUiGEA7EHMdtGh0WGxJbE5OQC+VRloWLiEJGRUdAxMrBxi-IIi3bCoAK7oRLDSEF5gzNwAbqiYUwJCwsQDw6P2Tkggru6e3tv+CACsQRlZCJEBzNEhd8fHJQEPkTmRNXUY2PhEpBQ09EYLDYHm6Sz6PGYaxGcGkYHQ6AwzBwlDw7D4GAAtj1lqshjDYJsfLtQV4fEcAhV8kFjnY6TljiEAjkQpFjudskFosxHqlcrEwtEcnEPiB6t8mn9WoCOugwHgINRppIZAoVBptHpDCYzJZrETtiT9uTEJFYsdmEEwscwmFIqU7LE7iEOSdHcwWcVHrEKuagqLxY1fi0Ae0WHKFUqJFJpKoAILKAAiWlUZjj+nUpgsVlktkcxLcpIOoCOIRyrtC1zLgttoUKQVidmOAa+Qea-zaQOYEcVzAIXYIEc4PFjCeTqfU6cz8YAchoADIGlyF42HRAVbl0umN6LROwJR5hV3RB7MQWhMIBfeRBLRd61MWtn7t6Vh7vy3v9jqD+XD3gowgxFHJMUzTDMtAABXnOMNC0RN5BndQlx2FdyDJNcEHiTctx3PcDxKV1aQiZJigFEIijNAIWwaZ8pVDLseyVL8WB-NFugAgggPjECJynSDoNg2cF2Qo00OLPx1xiAocMKPCmwIzJEAeCJzxCK1QmrZIagfLhUAgOAfEDWiQ07JgCz2MSTQQYggjsV1iAtZ0nOcssQmoiVgw7GVgS6HhzKLKyrxyWIojiFl7TyNlKmPGkCjiQoBQFQVoi0h8jMlEzvM6UFIXBUQ-MNVD0JLQJgmuGIKlZXIgii8tFJOYIeSKZ07FZWzQncts6NMnyct4PLumjMB-NXEqECSMIQlCyqIpq45ovqsJQmYYIloSaqQliDq0qfDKvLfEE-xxCFeGhUYRssjDnltG4ggCTakii44AiPRa6Ti0JHgeTTqh2mi9tfBiPwuZcLOKiSEBqitChuHJrSvSIavtEU-o8l96NlYHlSkC7waOXJIkta1bUvRlwnSeraW5UIvTiX1ns64z9qByM+yBXHxKOOHXQqC08kFebGViF6gn9VGusyt9GLZ78hzEDnAq5Y9SktK0UsbR48io8WmcBzHWeYvs5chDj5cKsHOcCPJCNpD11KW44toCK5m20oA */
     context: {
         dailyboardData: null,
         layoutData: null,
@@ -27,27 +27,20 @@ export const workspaceDailyboardMachine = setup({
     id: "workspace-dailyboard",
     initial: "waiting",
     states: {
-        "waiting": {
-            initial: "idle",
+        waiting: {
+            initial: "fetching",
             states: {
-                "idle": {
-                    on: {
-                        SOURCES_FETCH_REQUEST: {
-                            target: "fetching.sources"
-                        }
-                    }
-                },
-                "fetching": {
+                fetching: {
                     initial: "idle",
                     states: {
-                        "idle": {
+                        idle: {
                             on: {
                                 SOURCES_FETCH_REQUEST: {
                                     target: "sources"
                                 }
                             }
                         },
-                        "sources": {
+                        sources: {
                             invoke: {
                                 src: "fetchSources",
                                 id: "fetch-sources",
@@ -70,31 +63,26 @@ export const workspaceDailyboardMachine = setup({
                 }
             }
         },
-        "ready": {
+        ready: {
             initial: "idle",
-            states: {                
-                "idle": {
+            states: {
+                idle: {
                     on: {
                         SOURCES_FETCH_REQUEST: {
                             target: "#workspace-dailyboard.waiting.fetching.sources"
                         },
                         CARD_CREATE_REQUEST: {
-                            target: "creating.placing"
+                            target: "card.creating.placing"
                         }
                     }
                 },
-                "card": {
-                    initial: "idle",
+                card: {
+                    initial: "creating",
                     states: {
-                        "idle": {
-                        },
-                        "creating": {
-                            initial: "idle",
+                        creating: {
+                            initial: "placing",
                             states: {
-                                "idle": {
-                                    
-                                },
-                                "placing": {
+                                placing: {
                                     on: {
                                         CARD_CREATE_PLACE_DONE: {
                                             target: "#workspace-dailyboard.ready.idle"
@@ -106,9 +94,6 @@ export const workspaceDailyboardMachine = setup({
                                 }
                             },
                             on: {
-                                CARD_CREATE_PLACE_REQUEST: {
-                                    target: "creating.placing"
-                                },
                                 CARD_CREATE_CANCEL: {
                                     target: "#workspace-dailyboard.ready.idle"
                                 }
