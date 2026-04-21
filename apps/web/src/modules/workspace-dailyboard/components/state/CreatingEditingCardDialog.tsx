dd810cfcbd76f5849fbebe2d989e229d91442322
k4yr2 <kyrzkaya@gmail.com>
2026-04-21 23:57:56 +0300

    feat: enhance CreatingEditingCardDialog with dynamic card sizing and layout adjustments

--- apps/web/src/modules/workspace-dailyboard/components/state/CreatingEditingCardDialog.tsx
+++ apps/web/src/modules/workspace-dailyboard/components/state/CreatingEditingCardDialog.tsx
@@ -1,11 +1,16 @@
 import { Dialog, DialogContent, IconButton } from "@mui/material";
 import { useWorkspaceDailyboardContext } from "../../hooks/useWorkspaceDailyboardContext";
 import { isCreatingEditingCard } from "../../state/workspace-dailyboard.machine";
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import { useDailyboardContext } from "@/src/modules/ui-dailyboard/hooks/useDailyboardContext";
+import { GridSize } from "@/src/modules/shared/types/common";
 
 export function CreatingEditingCardDialog() {
     const { send, state } = useWorkspaceDailyboardContext();
+    const { metrics } = useDailyboardContext();
+
+    const [cardSpan, setCardSpan] = useState<GridSize | null>(null);
 
     const isOpen = useMemo(() => {
         return isCreatingEditingCard(state);
@@ -14,6 +19,22 @@ export function CreatingEditingCardDialog() {
     const handleClose = useCallback(() => {
         send({ type: "CARD_CREATE_CANCEL" });
     }, [send]);
+
+    useEffect(() => {
+        if(isOpen) {
+            setCardSpan({ colSpan: 12, rowSpan: 3 });
+        }
+    }, [isOpen]);
+
+    const cellStep = useMemo(() => {
+        return metrics.value?.layout.cellSize.grid || 0;
+    }, [metrics.value]);
+
+    const cardSize = useMemo(() => {
+        const width = cellStep * (cardSpan?.colSpan || 0);
+        const height = cellStep * (cardSpan?.rowSpan || 0);
+        return { width, height };
+    }, [cardSpan, cellStep]);
 
     return (
         <Dialog
@@ -32,14 +53,27 @@ export function CreatingEditingCardDialog() {
                 }
             }}
         >
-            <DialogContent style={{ backgroundColor: "transparent" }}>
+            <DialogContent style={{  
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "transparent" 
+            }}>
                 <IconButton 
                     onClick={handleClose}
                     style={{ position: "absolute", top: 16, right: 16, zIndex: 1, color: "white" }}
                 >
                     <CloseIcon />
                 </IconButton>
-                Hi
+                <div
+                    className="heute-card"
+                    style={{
+                        width: cardSize.width,
+                        height: cardSize.height,
+                    }}
+                >
+
+                </div>
             </DialogContent>
         </Dialog>
     )
