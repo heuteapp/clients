8aba1d0a4e57abc83acc28e32cb343b58bc08d9f
k4yr2 <kyrzkaya@gmail.com>
2026-04-21 20:02:35 +0300

    refactor: remove bullet points side interfaces from DailyboardCardSide type

--- apps/web/src/modules/dailyboard/types/dailyboard.data.types.ts
+++ apps/web/src/modules/dailyboard/types/dailyboard.data.types.ts
@@ -56,7 +56,6 @@ export enum DailyboardCardColor {
 
 export type DailyboardCardSide = 
     | DailyboardCardPlainTextSide 
-    | DailyboardCardBulletPointsSide;
 
 export interface DailyboardCardSideBase {
     type: "plain-text" | "bullet-points"
@@ -65,38 +64,4 @@ export interface DailyboardCardSideBase {
 export interface DailyboardCardPlainTextSide extends DailyboardCardSideBase {
     type: "plain-text";
     text: string;
-}
-
-export type DailyboardCardBulletPointsSide =
-    | DailyboardCardBulletPoints1x2Side
-    | DailyboardCardBulletPoints2x2Side
-    | DailyboardCardBulletPoints2x3Side;
-
-export interface DailyboardCardBulletPointsSideBase extends DailyboardCardSideBase {
-    type: "bullet-points";
-    layout: "1x2" | "2x2" | "2x3";
-}
-
-export interface DailyboardCardBulletPoints1x2Side extends DailyboardCardBulletPointsSideBase {
-    layout: "1x2";
-    pointUp: string;
-    pointDown: string;
-}
-
-export interface DailyboardCardBulletPoints2x2Side extends DailyboardCardBulletPointsSideBase {
-    layout: "2x2";
-    pointTopLeft: string;
-    pointTopRight: string;
-    pointBottomLeft: string;
-    pointBottomRight: string;
-}
-
-export interface DailyboardCardBulletPoints2x3Side extends DailyboardCardBulletPointsSideBase {
-    layout: "2x3";
-    pointTopLeft: string;
-    pointTopRight: string;
-    pointMiddleLeft: string;
-    pointMiddleRight: string;
-    pointBottomLeft: string;
-    pointBottomRight: string;
 }
