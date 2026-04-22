7e6f8a9a4f0087cb6eb4393a6e317c49a6fd5866
k4yr2 <kyrzkaya@gmail.com>
2026-04-22 03:30:32 +0300

    feat: enhance CreatingEditingCardDialog layout with zoom functionality and improved card display

--- apps/web/src/modules/workspace-dailyboard/components/state/CreatingEditingCardDialog.tsx
+++ apps/web/src/modules/workspace-dailyboard/components/state/CreatingEditingCardDialog.tsx
@@ -1,8 +1,9 @@
-import { Dialog, DialogContent, IconButton } from "@mui/material";
+import { Box, Dialog, DialogContent, IconButton } from "@mui/material";
 import { useWorkspaceDailyboardContext } from "../../hooks/useWorkspaceDailyboardContext";
 import { isCreatingEditingCard } from "../../state/workspace-dailyboard.machine";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import ZoomOutMapIcon from "@mui/icons-material/ZoomOutMap";
 import { useDailyboardContext } from "@/src/modules/ui-dailyboard/hooks/useDailyboardContext";
 import { GridSize } from "@/src/modules/shared/types/common";
 import { DailyboardCardDisplay } from "@/src/modules/ui-dailyboard/components/DailyboardCardDisplay";
@@ -68,14 +69,45 @@ export function CreatingEditingCardDialog() {
                 >
                     <CloseIcon />
                 </IconButton>
-                <DailyboardCardDisplay
-                    state={{
-                        content: cardContent!,
-                        isFrontFace: true,
-                        cardSpan: cardSpan!,
-                        cellStep
+                <Box
+                    sx={{
+                        position: "relative"
                     }}
-                />
+                >
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            inset: "auto 0 100% 0",
+                            height: cellStep,
+                            //border: "2px dashed rgba(255, 255, 255, 0.4)",
+                            borderRadius: 1,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <IconButton color="inherit"
+                            sx={{
+                                border: "2px solid rgba(255, 255, 255, 0.4)",
+                                borderRadius: "30%",
+                                overflow: "hidden",
+                                "& .MuiTouchRipple-child": {
+                                    borderRadius: "30%",
+                                }
+                            }}
+                        >
+                            <ZoomOutMapIcon />
+                        </IconButton>
+                    </Box>
+                    <DailyboardCardDisplay
+                        state={{
+                            content: cardContent!,
+                            isFrontFace: true,
+                            cardSpan: cardSpan!,
+                            cellStep
+                        }}
+                    />
+                </Box>
             </DialogContent>
         </Dialog>
     )
