c9577c42b183483da82c1637518861785d343f50
k4yr2 <kyrzkaya@gmail.com>
2026-04-22 16:06:51 +0300

    feat: restructure snapshot types by removing old CanvasSnapshot and UISnapshot interfaces and introducing a new snapshot.types.ts file

---
+++ apps/web/src/modules/shared/types/snapshot.types.ts
@@ -0,0 +1,32 @@
+import { DailyboardCardMaterial, DailyboardCardPlacement } from "../../dailyboard/types/dailyboard.data.types";
+import { GridDimensions, GridRect, Rect } from "./common"
+import { YYMMDDDate } from "./date.types";
+
+export interface BaseSnapshot {
+    rect: Rect;
+}
+
+export interface CanvasSnapshot extends BaseSnapshot {
+    canvasName: string;
+    canvasVersion: number;
+    dimensions: GridDimensions;
+    grids: CanvasGridSnapshot[];
+}
+
+export interface CanvasGridSnapshot extends BaseSnapshot {
+    gridKey: string;
+    gridRect: GridRect;
+}
+
+export interface DailyboardSnapshot extends BaseSnapshot {
+    categoryPath: string;
+    date: YYMMDDDate;
+    canvas: CanvasSnapshot;
+    cards: DailyboardCardSnapshot[];
+}
+
+export interface DailyboardCardSnapshot extends BaseSnapshot {
+    cardKey: string;
+    cardContent: DailyboardCardMaterial;
+    cardPlacement: DailyboardCardPlacement;
+}
