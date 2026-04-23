cbbf4b9ae80b26527482f21a6306504fbb3ce2c0
k4yr2 <kyrzkaya@gmail.com>
2026-04-23 13:39:18 +0300

    refactor: enhance CanvasGridContainer with context integration and lifecycle management

--- apps/web/src/modules/ui-canvas/components/CanvasGridContainer.tsx
+++ apps/web/src/modules/ui-canvas/components/CanvasGridContainer.tsx
@@ -1,12 +1,24 @@
 import style from "@/src/modules/ui-canvas/styles/canvas.module.scss"
 
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { CanvasGridContainerProps } from "../types/canvas.props";
 import { CanvasGridSection } from "./CanvasGridSection";
+import { useCanvasContext } from "../hooks/useCanvasContext";
 
 export function CanvasGridContainer(props : CanvasGridContainerProps) {
     const ref = useRef<HTMLDivElement>(null);
     const { grids } = props;
+
+    const context = useCanvasContext();
+    const { registry } = context!;
+
+    useLayoutEffect(() => {
+        registry.registerCanvasGridContainer(ref, props);
+
+        return () => {
+            registry.unregisterCanvasGridContainer()
+        }
+    }, [registry])
 
     return (
         <div
