783f51d04febce9175f027898d7f628eb4fc327b
k4yr2 <kyrzkaya@gmail.com>
2026-04-23 23:40:55 +0300

    feat: refactor TracingProvider to use TracingContext for context value

--- apps/web/src/modules/t-shared/providers/TracingProvider.tsx
+++ apps/web/src/modules/t-shared/providers/TracingProvider.tsx
@@ -1,11 +1,17 @@
+import { useMemo } from "react";
+import { TracingContext } from "../contexts/tracing.context";
 import { TracingProviderProps } from "../types/props.types";
 
 export function TracingProvider({ children }: TracingProviderProps) {
-
+    const contextValue = useMemo(() => {
+        return {
+            components: new Map(),
+        };
+    }, []);
 
     return (
-        <>
+        <TracingContext.Provider value={contextValue}>
             {children}
-        </>
+        </TracingContext.Provider>
     );
 }
