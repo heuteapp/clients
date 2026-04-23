9491395e49a67d1e972b5f92f7a14fb1134c2e99
k4yr2 <kyrzkaya@gmail.com>
2026-04-23 23:19:10 +0300

    feat: add TracingContextValue and TracingItemData interfaces

---
+++ apps/web/src/modules/t-shared/types/context.types.ts
@@ -0,0 +1,10 @@
+import { Rect } from "../../d-shared/types/common";
+
+export interface TracingContextValue {
+    components: Map<string, TracingItemData>;
+}
+
+export interface TracingItemData {
+    rect: Rect;
+    props: any;
+}
