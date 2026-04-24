8f391f6e23dec91b6452ecb69525f582ca411551
k4yr2 <kyrzkaya@gmail.com>
2026-04-24 20:15:01 +0300

    feat: extend board types to include dailyboard structures and models

--- apps/web/src/heute-store/types/board.types.ts
+++ apps/web/src/heute-store/types/board.types.ts
@@ -31,9 +31,9 @@ export type BoardBaseSource = BoardBase;
 export type BoardCardBaseSource = BoardCardBase;
 
 export interface StoredBoardItem<
-    TDailyboardCard extends StoredBoardCardItem = StoredBoardCardItem
+    TBoardCard extends StoredBoardCardItem = StoredBoardCardItem
 > extends StoredItem, BoardBase {
-    cards: TDailyboardCard[];
+    cards: TBoardCard[];
 }
 
 export interface StoredBoardCardItem extends StoredItem, BoardCardBase {
@@ -64,9 +64,9 @@ export type BoardModelSource = BoardModel;
 export type BoardCardModelSource = BoardCardModel;
 
 export interface StoredBoardModel<
-    TDailyboardCard extends StoredBoardCardModel = StoredBoardCardModel
-> extends StoredBoardItem<TDailyboardCard>, BoardModel {
-    cards: TDailyboardCard[];
+    TBoardCard extends StoredBoardCardModel = StoredBoardCardModel
+> extends StoredBoardItem<TBoardCard>, BoardModel {
+    cards: TBoardCard[];
 }
 
 export interface StoredBoardCardModel extends StoredBoardCardItem, BoardCardModel {
@@ -76,3 +76,69 @@ export interface StoredBoardCardModel extends StoredBoardCardItem, BoardCardMode
 export type StoredBoardModelData<TBase extends StoredBoardModel = StoredBoardModel> = StoredBoardItemData<TBase>
 
 export type StoredBoardCardModelData<TBase extends StoredBoardCardModel = StoredBoardCardModel> = StoredBoardCardItemData<TBase>;
+
+//
+
+import { DailyboardBase, DailyboardCardBase } from "@/src/modules/d-board/types/board.base.types";
+
+export interface DailyboardBaseState extends BoardBaseState<
+    DailyboardBaseSource,
+    DailyboardCardBaseSource,
+    StoredDailyboardItem,
+    StoredDailyboardItemData,
+    StoredDailyboardCardItem,
+    StoredDailyboardCardItemData
+> {
+
+}
+
+export type DailyboardBaseSource = DailyboardBase;
+
+export type DailyboardCardBaseSource = DailyboardCardBase;
+
+export interface StoredDailyboardItem<
+    TDailyboardCard extends StoredDailyboardCardItem = StoredDailyboardCardItem
+> extends StoredBoardItem<TDailyboardCard>, DailyboardBase {
+    cards: TDailyboardCard[];
+}
+
+export interface StoredDailyboardCardItem extends StoredBoardCardItem, DailyboardCardBase {
+
+}
+
+export type StoredDailyboardItemData<TBase extends StoredDailyboardItem = StoredDailyboardItem> = StoredBoardItemData<TBase>
+
+export type StoredDailyboardCardItemData<TBase extends StoredDailyboardCardItem = StoredDailyboardCardItem> = StoredBoardCardItemData<TBase>;
+
+//
+
+import { DailyboardModel, DailyboardCardModel} from "@/src/modules/d-board/types/board.model.types";
+
+export interface DailyboardModelState extends BoardBaseState<
+    DailyboardModelSource,
+    DailyboardCardModelSource,
+    StoredDailyboardItem,
+    StoredDailyboardItemData,
+    StoredDailyboardCardItem,
+    StoredDailyboardCardItemData
+> {
+
+}
+
+export type DailyboardModelSource = DailyboardModel;
+
+export type DailyboardCardModelSource = DailyboardCardModel;
+
+export interface StoredDailyboardModel<
+    TDailyboardCard extends StoredDailyboardCardModel = StoredDailyboardCardModel
+> extends StoredBoardItem<TDailyboardCard>, DailyboardModel {
+    cards: TDailyboardCard[];
+}
+
+export interface StoredDailyboardCardModel extends StoredBoardCardItem, DailyboardCardModel {
+
+}
+
+export type StoredDailyboardModelData<TBase extends StoredDailyboardModel = StoredDailyboardModel> = StoredBoardItemData<TBase>
+
+export type StoredDailyboardCardModelData<TBase extends StoredDailyboardCardModel = StoredDailyboardCardModel> = StoredBoardCardItemData<TBase>;
