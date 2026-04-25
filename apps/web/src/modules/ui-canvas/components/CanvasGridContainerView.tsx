217b601554b7a6c1ab44525278f73005ef28b4f2
k4yr2 <kyrzkaya@gmail.com>
2026-04-25 20:57:07 +0300

    refactor: Simplify CanvasGridContainer by removing unused logic and integrating CanvasGridContainerView for better structure

---
+++ apps/web/src/modules/ui-canvas/components/CanvasGridContainerView.tsx
@@ -0,0 +1,46 @@
+import style from "@/src/modules/ux-canvas/styles/canvas.module.scss"
+import clsx from "clsx";
+
+import { CanvasGridContainerViewProps } from "../types/props.types";
+import { useMemo } from "react";
+import { CanvasGridSectionView } from "./CanvasGridSectionView";
+
+//
+
+export function CanvasGridContainerView({ ref, state, className, sx, render }: CanvasGridContainerViewProps) {
+    const matrix = useMemo(() => {
+        const result = Array.from({ length: state.rowCount }, () =>
+            Array.from({ length: state.colCount }, () => ".")
+        );
+
+        state.areas.forEach(s => {
+            const { rowIndex, colIndex, rowSpan, colSpan } = s.position;
+
+            for (let r = 0; r < rowSpan; r++) {
+                for (let c = 0; c < colSpan; c++) {
+                    result[rowIndex - 1 + r][colIndex - 1 + c] = s.areaName;
+                }
+            }
+        });
+
+        return result;
+    }, [state.rowCount, state.colCount]);
+
+    const gridTemplateAreas = useMemo(() => {
+        return matrix.map(row => `"${row.join(" ")}"`).join(" ");
+    }, [matrix]);
+
+    return (  
+        <div
+            ref={ref}
+            className={clsx(style.gridContainer, ...(className || []))}
+            style={{
+                gridTemplateColumns: `repeat(${state.colCount}, var(--canvas-cell-size))`,
+                gridTemplateRows: `repeat(${state.rowCount}, var(--canvas-cell-size))`,
+                gridTemplateAreas
+            }}
+        > 
+            {render ? render(state) : state.areas.map(s => <CanvasGridSectionView key={s.areaName} state={s} />)}
+        </div>       
+    )
+}
--- apps/web/src/modules/ux-canvas/components/CanvasGridContainer.tsx
+++ apps/web/src/modules/ux-canvas/components/CanvasGridContainer.tsx
@@ -1,53 +1,27 @@
-import style from "@/src/modules/ux-canvas/styles/canvas.module.scss"
-
-import { useMemo, useRef } from "react";
+import { useRef } from "react";
 import { CanvasGridContainerProps } from "../types/canvas.props";
-import { CanvasGridSection } from "./CanvasGridSection";
 import { TracedUniqueItem } from "../../t-core/components/TracedUniqueItem";
+import { CanvasGridContainerView } from "../../ui-canvas/components/CanvasGridContainerView";
 
 export function CanvasGridContainer({ colCount, rowCount, gridSources }: CanvasGridContainerProps) {
     const ref = useRef<HTMLDivElement>(null);
-
-    const matrix = useMemo(() => {
-        const result = Array.from({ length: rowCount }, () =>
-            Array.from({ length: colCount }, () => ".")
-        );
-
-        gridSources.forEach(s => {
-            const { rowIndex, colIndex, rowSpan, colSpan } = s.position;
-
-            for (let r = 0; r < rowSpan; r++) {
-                for (let c = 0; c < colSpan; c++) {
-                    result[rowIndex - 1 + r][colIndex - 1 + c] = s.name;
-                }
-            }
-        });
-
-        return result;
-    }, [rowCount, colCount]);
-
-    const gridTemplateAreas = useMemo(() => {
-        return matrix.map(row => `"${row.join(" ")}"`).join(" ");
-    }, [matrix]);
 
     return (
         <TracedUniqueItem
             type="canvas-grid-container"
             ref={ref}
         >
-            <div
+            <CanvasGridContainerView 
                 ref={ref}
-                className={style.gridContainer}
-                style={{
-                    gridTemplateColumns: `repeat(${colCount}, var(--canvas-cell-size))`,
-                    gridTemplateRows: `repeat(${rowCount}, var(--canvas-cell-size))`,
-                    gridTemplateAreas
+                state={{
+                    colCount,
+                    rowCount,
+                    areas: gridSources.map(s => ({
+                        areaName: s.name,
+                        position: s.position,
+                    }))
                 }}
-            > 
-                {gridSources.map((grid) => (
-                    <CanvasGridSection key={grid.name} src={grid}/>
-                ))}
-            </div>        
+            />
         </TracedUniqueItem>
     )
 }
