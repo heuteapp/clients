8cb4201686fb1733b8b1a3eb6dd3b929008785fe
k4yr2 <kyrzkaya@gmail.com>
2026-04-25 20:16:52 +0300

    refactor: Update CanvasRoot, CanvasGridContainer, and CanvasGridSection view props for improved type consistency

--- apps/web/src/modules/ui-canvas/types/props.types.ts
+++ apps/web/src/modules/ui-canvas/types/props.types.ts
@@ -1,13 +1,13 @@
-import { SimpleViewProps } from "../../ui-base/types/props.types";
+import { RichViewProps, SimpleViewProps } from "../../ui-base/types/props.types";
 import { CanvasGridContainerViewState, CanvasGridItemViewState, CanvasGridSectionViewState, CanvasRootViewState } from "./view.types";
 
-export interface CanvasRootViewProps extends SimpleViewProps<CanvasRootViewState> {
+export interface CanvasRootViewProps extends RichViewProps<CanvasRootViewState, "gridContainer" | "gridSection" | "gridItem"> {
 }
 
-export interface CanvasGridContainerViewProps extends SimpleViewProps<CanvasGridContainerViewState> {
+export interface CanvasGridContainerViewProps extends RichViewProps<CanvasGridContainerViewState, "gridContainer" | "gridItem"> {
 }
 
-export interface CanvasGridSectionViewProps extends SimpleViewProps<CanvasGridSectionViewState> {
+export interface CanvasGridSectionViewProps extends RichViewProps<CanvasGridSectionViewState, "gridItem"> {
 }
 
 export interface CanvasGridItemViewProps extends SimpleViewProps<CanvasGridItemViewState> {
