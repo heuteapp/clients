267586f7dd2960074e70a6cda78ef6d215646dee
k4yr2 <kyrzkaya@gmail.com>
2026-04-25 20:12:26 +0300

    refactor: Consolidate view state interfaces for improved organization and clarity

--- apps/web/src/modules/ui-canvas/types/props.types.ts
+++ apps/web/src/modules/ui-canvas/types/props.types.ts
@@ -1,5 +1,14 @@
 import { SimpleViewProps } from "../../ui-base/types/props.types";
-import { CanvasGridItemViewState } from "./view.types";
+import { CanvasGridContainerViewState, CanvasGridItemViewState, CanvasGridSectionViewState, CanvasRootViewState } from "./view.types";
+
+export interface CanvasRootViewProps extends SimpleViewProps<CanvasRootViewState> {
+}
+
+export interface CanvasGridContainerViewProps extends SimpleViewProps<CanvasGridContainerViewState> {
+}
+
+export interface CanvasGridSectionViewProps extends SimpleViewProps<CanvasGridSectionViewState> {
+}
 
 export interface CanvasGridItemViewProps extends SimpleViewProps<CanvasGridItemViewState> {
 }
--- apps/web/src/modules/ui-canvas/types/view.types.ts
+++ apps/web/src/modules/ui-canvas/types/view.types.ts
@@ -1,3 +1,12 @@
+export interface CanvasRootViewState {
+}
+
+export interface CanvasGridContainerViewState {
+}
+
+export interface CanvasGridSectionViewState {
+}
+
 export interface CanvasGridItemViewState {
     colSpan: number;
     rowSpan: number;
