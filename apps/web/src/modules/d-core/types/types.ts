cd36f72e633bd79e6e24652df1c45a2ff4f59eff
k4yr2 <kyrzkaya@gmail.com>
2026-04-26 17:48:46 +0300

    refactor: Add OmitKeysByPrefix and GetNestedValue types for improved type handling and clarity

--- apps/web/src/modules/d-core/types/types.ts
+++ apps/web/src/modules/d-core/types/types.ts
@@ -24,3 +24,20 @@ export type FlattenKeys<T> =
 export type FilterKeysByPrefix<T, Prefix extends string> = Simplify<{
   [K in keyof T as K extends `${Prefix}${string}` ? K : never]: T[K]
 }>;
+
+export type OmitKeysByPrefix<T, Prefix extends string> = Simplify<{
+  [K in keyof T as K extends `${Prefix}${string}` ? never : K]: T[K]
+}>;
+
+export type GetNestedValue<
+  T,
+  K extends string,
+  LeafValue = unknown,
+  BranchValue = Record<string, any>
+> = K extends keyof T
+  ? T[K] extends BranchValue | LeafValue
+    ? T[K]
+    : never
+  : T extends Record<string, infer U>
+  ? GetNestedValue<U, K, LeafValue, BranchValue>
+  : never;
