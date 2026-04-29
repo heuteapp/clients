62965c71859dbdffb2b9ec6acf239c20ad378042
k4yr2 <kyrzkaya@gmail.com>
2026-04-30 02:41:47 +0300

    refactor: Restructure ViewProps and ViewParams interfaces for improved clarity and consistency in component rendering

--- apps/web/src/modules/t-view/types/view.types.ts
+++ apps/web/src/modules/t-view/types/view.types.ts
@@ -1,12 +1,27 @@
 import { SxProps, Theme } from "@mui/system";
 
 export interface ViewProps {
-    sx?: SxProps<Theme>;
+    ref?: React.Ref<HTMLDivElement | null>;
+    state: ViewState;
     classNames?: string[];
+    styles?: React.CSSProperties;
+    sx?: SxProps<Theme>;
     children?: React.ReactNode;
 }
 
+export interface ViewState {
+    [key: string]: any;
+}
+
+export interface ViewOverrides {
+    classNames: string[];
+    styles: React.CSSProperties;
+    sx: SxProps<Theme>;
+}
+
 export interface ViewParams {
-    props: ViewProps;
-    content: (def: () => React.ReactNode) => React.ReactNode;
+    ref: React.Ref<HTMLDivElement | null> | null;
+    state: ViewState;
+    overrides: ViewOverrides;
+    content: (def?: () => React.ReactNode) => React.ReactNode;
 }
--- apps/web/src/modules/t-view/utils/view.utils.tsx
+++ apps/web/src/modules/t-view/utils/view.utils.tsx
@@ -1,14 +1,27 @@
+import { useCallback, useMemo } from "react";
 import { ViewParams, ViewProps } from "../types/view.types";
 
 export const VIEW = <TProps extends ViewProps>(render: (params: ViewParams) => React.ReactNode) => {
   return (props: TProps) => {
-    const content = (def: () => React.ReactNode) => {
+    const ref = props.ref || null;
+
+    const state = props.state;
+
+    const overrides = useMemo(() => {
+      return {
+        classNames: props.classNames || [],
+        styles: props.styles || {},
+        sx: props.sx || {},
+      };
+    }, [props.classNames, props.styles, props.sx]);
+
+    const content = useCallback((def?: () => React.ReactNode) => {
       if (props.children) {
         return props.children;
       }
-      return def();
-    };
+      return def?.() || null;
+    }, [props.children]);
 
-    return render({ props, content });
+    return render({ ref, state, overrides, content });
   };
 };
